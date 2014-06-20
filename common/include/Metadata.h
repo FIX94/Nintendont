7388730781a67e6c8d6abc392ad2e2c89411eea7
Howard_M_Busch@yahoo.com <Howard_M_Busch@yahoo.com@6acfca08-c3de-247c-4448-9f1a92385553>
2014-06-20 17:04:10 +0000

    -Fixed reference  to old website (Thanks Naxil)

--- common/include/Metadata.h
+++ common/include/Metadata.h
@@ -5,7 +5,7 @@
 #define META_NAME			"Nintendont"
 #define META_AUTHOR			"crediar"
 
-#define META_LONG1			"Commiters: JoostinOnline, Greyrogue, Howard, Cyan, Fix94 \r\n\r\n Project website: https://nintendont.googlecode.com/ "
+#define META_LONG1			"Commiters: JoostinOnline, Greyrogue, Howard, Cyan, Fix94 \r\n\r\n Project website: https://nintendon-t.googlecode.com/ "
 #define META_LONG2			"Nintendont allows you to run GameCube games on a Wii or Wii U from an SD or HDD device."
 #define META_SHORT			"Gamecube Loader"
 
