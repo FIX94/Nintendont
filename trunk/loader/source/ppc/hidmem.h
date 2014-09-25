9dd5cdd01634990de0436d68c710c3f3ef3de0aa
Howard_M_Busch@yahoo.com <Howard_M_Busch@yahoo.com@6acfca08-c3de-247c-4448-9f1a92385553>
2014-09-25 18:24:10 +0000

    -Fixed HID controller analog triggers being broken since v2.158

--- trunk/loader/source/ppc/hidmem.h
+++ trunk/loader/source/ppc/hidmem.h
@@ -2,5 +2,5 @@
 #ifndef _HIDMEM_H_
 #define _HIDMEM_H_
 static controller *HID_CTRL = (controller*)0x93005000;
-static u8 *HID_Packet = (u8*)0x930050E0;
+static u8 *HID_Packet = (u8*)0x930050F0;
 #endif
