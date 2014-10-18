dab54dac1e01efaece9ead729aa3a450c3d5165b
JoostinOnline@gmail.com <JoostinOnline@gmail.com@6acfca08-c3de-247c-4448-9f1a92385553>
2014-10-18 18:54:27 +0000

    -Nintenont updater now shows what revision is being downloaded, and makes sure you don't already have the latest version -Additional minor changes to the updater

--- loader/source/update.c
+++ loader/source/update.c
@@ -12,13 +12,38 @@
 #include "global.h"
 #include "http.h"
 #include "update.h"
+#include "../../common/include/NintendontVersion.h"
+
+
+static inline bool LatestVersion(int *major, int *minor) {
+	u32 http_status = 0;
+	u8* outbuf = NULL;
+	u32 filesize;
+	
+	http_request("http://nintendon-t.googlecode.com/svn/trunk/common/include/NintendontVersion.h", 1 << 10);
+	
+	http_get_result(&http_status, &outbuf, &filesize); 
+	
+	if (((int)*outbuf & 0xF0000000) == 0xF0000000) 
+	{
+		if (outbuf != NULL) free(outbuf);
+		return false;
+	}
+	sscanf((char*)outbuf, " #ifndef %*s #define %*s #define NIN_MAJOR_VERSION %i #define NIN_MINOR_VERSION %i", major, minor);
+
+	if (outbuf != NULL) free(outbuf);
+	return (*major + *minor) > 0;
+
+}
 
 s32 UpdateNintendont(const char* directory)  {
 	gprintf("Updating\r\n");
-	PrintFormat( MENU_POS_X, MENU_POS_Y + 20*2, "Updating Nintendont");
-	int ret;
+	int line = 1;
+	PrintFormat( MENU_POS_X, MENU_POS_Y + 20*line, "Updating Nintendont");
+	line++;
+	int ret, major = 0, minor = 0;
 	u32 http_status = 0;
-	u8* outbuf;
+	u8* outbuf = NULL;
 	u32 filesize;
 	FILE *file;
 	gprintf("Update directory: %s", directory);
@@ -28,7 +53,16 @@ s32 UpdateNintendont(const char* directory)  {
 		goto end;
 	}
 	gprintf("Network Initialized\r\n");
-	PrintFormat( MENU_POS_X, MENU_POS_Y + 20*3, "Downloading");
+	PrintFormat( MENU_POS_X, MENU_POS_Y + 20*line, "Network Initialized");
+	line++;
+	ret = LatestVersion(&major, &minor);
+	if (ret && (major <= NIN_MAJOR_VERSION) && (minor <= NIN_MINOR_VERSION)) {
+		PrintFormat( MENU_POS_X, MENU_POS_Y + 20*line, "You already have the lastest version");
+		line++;
+		goto end;
+	}
+	PrintFormat( MENU_POS_X, MENU_POS_Y + 20*line, "Downloading v%i.%i", major, minor);
+	line++;
 	int i;
 	for (i = 0; i < 10; i++) {
 		ret = http_request("http://nintendon-t.googlecode.com/svn/trunk/loader/loader.dol", 1 << 30); // I doubt it will ever go over 1MB
@@ -46,7 +80,8 @@ s32 UpdateNintendont(const char* directory)  {
 		ret = -2;
 		goto end;
 	}
-	PrintFormat( MENU_POS_X, MENU_POS_Y + 20*4, "Download Complete");
+	PrintFormat( MENU_POS_X, MENU_POS_Y + 20*line, "Download Complete");
+	line++;
 	if(strlen(directory) > 0) {
 		file = fopen(directory, "wb");
 	} else {
@@ -60,12 +95,12 @@ s32 UpdateNintendont(const char* directory)  {
 	} else {
 		fwrite(outbuf, filesize, 1, file);
 		fclose(file);
-		PrintFormat( MENU_POS_X, MENU_POS_Y + 20*5, "Update Complete");
+		PrintFormat( MENU_POS_X, MENU_POS_Y + 20*line, "Update Complete");
 	}
 
 end:
-	free(outbuf);
-	if (ret != 1) PrintFormat( MENU_POS_X, MENU_POS_Y + 20*5, "Update Error: %i", ret);
+	if (outbuf != NULL) free(outbuf);
+	if (ret != 1) PrintFormat( MENU_POS_X, MENU_POS_Y + 20*line, "Update Error: %i", ret);
 	net_deinit();
 	sleep(5);
 	return ret;
