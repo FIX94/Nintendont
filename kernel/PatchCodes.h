ba8dd287a7ac02954348d6aac421bc85efb4a332
fix94.1@gmail.com <fix94.1@gmail.com@6acfca08-c3de-247c-4448-9f1a92385553>
2014-10-29 19:11:45 +0000

    -added a different approach to fix ARStartDMA issues for certain games, check the Patch.c diff to see which games are affected

--- kernel/PatchCodes.h
+++ kernel/PatchCodes.h
@@ -20,6 +20,7 @@
 #include "asm/ARStartDMA_PM.h"
 #include "asm/ARStartDMA_NFS.h"
 #include "asm/ARStartDMA_Hook.h"
+#include "asm/__ARHandler.h"
 #include "asm/SIGetType.h"
 #include "asm/__OSReadROM.h"
 #include "asm/FakeInterrupt.h"
