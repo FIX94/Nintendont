bf57d5cbad1f7ecb47b48d070dbb79e7ef6fc7a9
FIX94 <fix94.1@gmail.com>
2015-05-31 14:34:55 +0200

    -updated the meta.xml to point to the new github location

--- common/include/Metadata.h
+++ common/include/Metadata.h
@@ -3,9 +3,9 @@
 #define __META_DATA_H__
 
 #define META_NAME			"Nintendont"
-#define META_AUTHOR			"crediar"
+#define META_AUTHOR			"FIX94, crediar"
 
-#define META_LONG1			"Commiters: JoostinOnline, Greyrogue, Howard, Cyan, Fix94 \r\n\r\n Project website: https://nintendon-t.googlecode.com/ "
+#define META_LONG1			"Commiters: JoostinOnline, GreyRogue, Howard, Cyan \r\n\r\n Project website: https://github.com/FIX94/Nintendont "
 #define META_LONG2			"Nintendont allows you to run GameCube games on a Wii or Wii U from an SD or HDD device."
 #define META_SHORT			"Gamecube Loader"
 
