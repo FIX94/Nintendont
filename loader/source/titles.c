e34d462195ddc310620756e4006d95da8dc02d39
David Korth <gerbilsoft@gerbilsoft.com>
2016-05-24 20:50:04 -0400

    [loader] titles.c: Use a binary search for titles.
    I performed some benchmarks for loading the game list:
    
    - master (v399, gpt-support-v3):       0.964s
    - menuhax before switching to fgets(): 1.296s
    - menuhax after switching to fgets():  1.298s
    - menuhax after binary search:         1.295s
    
    The menuhax branch seems to have added 0.3s of extra loading time,
    which isn't too significant. I'll try to cut it down with some
    more optimizations.

--- loader/source/titles.c
+++ loader/source/titles.c
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <stdio.h>
 #include <malloc.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "font.h"
 #include "global.h"
@@ -56,11 +57,22 @@ static const SpecialTitles_t TriforceTitles[] = {
 	{"GVS46J", "Virtua Striker 4 Ver.2006"},
 	{"GVS46E", "Virtua Striker 4 Ver.2006"}
 };
-	
+
 
 static char __title_list[MAX_TITLES][LINE_LENGTH] = {{0}};
 static u32 title_count = 0;
 static bool loaded = false;
+
+/**
+ * qsort() comparison function for titles.
+ * Checks the ID3.
+ * @param p1
+ * @param p2
+ */
+static int compare_title(const void *p1, const void *p2)
+{
+       return strncmp((const char*)p1, (const char*)p2, 3);
+}
 
 s32 LoadTitles(void)
 {
@@ -99,6 +111,10 @@ s32 LoadTitles(void)
 		}
 	} while (!feof(titles_txt) && title_count < MAX_TITLES);
 
+	// Sort the titles so we can do a binary search later.
+	// __title_list[] format: "ID3-Title"
+	qsort(__title_list, title_count, LINE_LENGTH, compare_title);
+
 	fclose(titles_txt);
 	return title_count;
 }
@@ -111,20 +127,38 @@ s32 LoadTitles(void)
  * WARNING: DO NOT FREE the returned title!
  */
 const char *SearchTitles(const char *titleID) {
-	// FIXME: This is a linear search, which can be slow...
-	if (!loaded) return NULL;
+	if (!loaded) {
+		// Titles haven't been loaded.
+		return NULL;
+	}
+
+	// Check for Triforce arcade games first.
 	int i;
-	for(i=0; i < MAX_ELEMENTS(TriforceTitles); i++) { // Check for Triforce arcade games first
+	for (i = 0; i < MAX_ELEMENTS(TriforceTitles); i++) {
 		if (!strncmp(titleID, TriforceTitles[i].titleID, 6)) {
 			gprintf("Found special title %.6s, replacing name with %s\r\n", titleID, TriforceTitles[i].titleName);
 			return TriforceTitles[i].titleName;
 		}
 	}
-	for(i=0; i < title_count; i++) {
-		// __title_list[] format: ID3-Title
-		if (!strncmp(titleID, __title_list[i], 3)) {
-			return __title_list[i] + 4;
+
+	// Do a binary search for the ID3.
+	// __title_list[] format: "ID3-Title"
+	// Reference: http://www.programmingsimplified.com/c/source-code/c-program-binary-search
+	// TODO: Do timing tests.
+	int first = 0;
+	int last = title_count - 1;
+	int middle = (first + last) / 2;
+	while (first <= last) {
+		int res = strncmp(__title_list[middle], titleID, 3);
+		if (res < 0) {
+			first = middle + 1;
+		} else if (res > 0) {
+			last = middle - 1;
+		} else {
+			// Found a match.
+			return __title_list[middle] + 4;
 		}
+		middle = (first + last) / 2;
 	}
 
 	// Title not found.
