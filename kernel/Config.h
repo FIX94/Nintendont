FIX94/Nintendont

commit a5ff0a7fd1db3442d872446513b165afc9219394

[kernel] Config.h: Actually inline the accessor functions.
Even though they were marked inline in Config.c, they can't actually
be inlined unless they're placed in Config.h. (Well, we could enable
link-time optimization, but that adds its own set of problems.)

Related changes:
- Use 'static inline', since the functions are now in a header.
- Changed the return types of the path accessors to 'const char*'.

kernel/Config.h
@@ -42,18 +42,69 @@ enum SystemLanguage {	Japanese,
 
 void ConfigSyncBeforeRead( void );
 void ConfigInit( void );
-char *ConfigGetGamePath( void );
-char *ConfigGetCheatPath( void );
-bool ConfigGetConfig( u32 Config );
-u32 ConfigGetVideoMode( void );
-u32 ConfigGetLanguage( void );
-u32 ConfigGetMaxPads(void);
-u32 ConfigGetGameID(void);
-s8 ConfigGetVideoScale(void);
-s8 ConfigGetVideoOffset(void);
-u32 ConfigGetMemcardCode(void);
-u32 ConfigGetMemcardSize(void);
-void ConfigSetMemcardBlocks(u32 MemCardBlocks);
-//bool IsWiiU;
+
+// Nintendont configuration.
+static NIN_CFG *const ncfg = (NIN_CFG*)0x13002900;
+
+static inline const char *ConfigGetGamePath(void)
+{
+	return ncfg->GamePath;
+}
+
+static inline const char *ConfigGetCheatPath(void)
+{
+	return ncfg->CheatPath;
+}
+
+static inline bool ConfigGetConfig(u32 Config)
+{
+	return !!(ncfg->Config&Config);
+}
+
+static inline u32 ConfigGetVideoMode(void)
+{
+	return ncfg->VideoMode;
+}
+
+static inline u32 ConfigGetLanguage(void)
+{
+	return ncfg->Language;
+}
+
+static inline u32 ConfigGetMaxPads(void)
+{
+	return ncfg->MaxPads;
+}
+
+static inline u32 ConfigGetGameID(void)
+{
+	return ncfg->GameID;
+}
+
+static inline s8 ConfigGetVideoScale(void)
+{
+	return ncfg->VideoScale;
+}
+
+static inline s8 ConfigGetVideoOffset(void)
+{
+	return ncfg->VideoOffset;
+}
+
+static inline u32 ConfigGetMemcardCode(void)
+{
+	return MEM_CARD_CODE(ncfg->MemCardBlocks);
+}
+
+static inline u32 ConfigGetMemcardSize(void)
+{
+	return MEM_CARD_SIZE(ncfg->MemCardBlocks);
+}
+
+static inline void ConfigSetMemcardBlocks(u32 MemCardBlocks)
+{
+	ncfg->MemCardBlocks = MemCardBlocks;
+	sync_after_write(&(ncfg->MemCardBlocks), sizeof(ncfg->MemCardBlocks));
+}
 
 #endif

kernel/Config.c
@@ -2,8 +2,6 @@
 #include "debug.h"
 #include "ff_utf8.h"
 
-NIN_CFG *ncfg = (NIN_CFG*)0x13002900;
-
 void ConfigSyncBeforeRead( void )
 {
 	sync_before_read(ncfg, sizeof(NIN_CFG));
@@ -47,53 +45,4 @@ void ConfigInit( void )
 	//{
 	//	ncfg->Config &= ~(NIN_CFG_CHEATS|NIN_CFG_DEBUGGER|NIN_CFG_DEBUGWAIT);
 	//}
-}
-inline char *ConfigGetGamePath( void )
-{
-	return ncfg->GamePath;
-}
-inline char *ConfigGetCheatPath( void )
-{
-	return ncfg->CheatPath;
-}
-inline bool ConfigGetConfig( u32 Config )
-{
-	return !!(ncfg->Config&Config);
-}
-inline u32 ConfigGetVideoMode( void )
-{
-	return ncfg->VideoMode;
-}
-inline u32 ConfigGetLanguage( void )
-{
-	return ncfg->Language;
-}
-inline u32 ConfigGetMaxPads(void)
-{
-	return ncfg->MaxPads;
-}
-inline u32 ConfigGetGameID(void)
-{
-	return ncfg->GameID;
-}
-inline s8 ConfigGetVideoScale(void)
-{
-	return ncfg->VideoScale;
-}
-inline s8 ConfigGetVideoOffset(void)
-{
-	return ncfg->VideoOffset;
-}
-inline u32 ConfigGetMemcardCode(void)
-{
-	return MEM_CARD_CODE(ncfg->MemCardBlocks);
-}
-inline u32 ConfigGetMemcardSize(void)
-{
-	return MEM_CARD_SIZE(ncfg->MemCardBlocks);
-}
-inline void ConfigSetMemcardBlocks(u32 MemCardBlocks)
-{
-	ncfg->MemCardBlocks = MemCardBlocks;
-	sync_after_write(&(ncfg->MemCardBlocks), sizeof(ncfg->MemCardBlocks));
 }

commit d00029e3994559c669e8514b6a02d42843a29c41

[kernel] Fixed some 'const char*' issues.
These were caused by commit a5ff0a7fd1db3442d872446513b165afc9219394.
([kernel] Config.h: Actually inline the accessor functions.)

- ConfigGetGamePath(): DIChangeDisc() modifies this path, so it
  has to be returned as 'char*'.

- Patch.c:
  - Check_Cheats(): Use 'const char*'.
  - fileExist(): Changed the parameter to 'const char*'.

kernel/Config.h
@@ -46,7 +46,8 @@ void ConfigInit( void );
 // Nintendont configuration.
 static NIN_CFG *const ncfg = (NIN_CFG*)0x13002900;
 
-static inline const char *ConfigGetGamePath(void)
+// NOTE: DIChangeDisc() modifies this path.
+static inline char *ConfigGetGamePath(void)
 {
 	return ncfg->GamePath;
 }

kernel/Patch.c
@@ -1118,12 +1118,17 @@ static int GotoFuncEnd(int i, u32 Buffer)
 	return i;
 }
 
-static bool fileExist( char *path )
+/**
+ * Does the specified file exist?
+ * @param path Filename.
+ * @return True if the file exists; false if it doesn't.
+ */
+static bool fileExist(const char *path)
 {
 	FIL fd;
-	if( f_open_char( &fd, path, FA_OPEN_EXISTING|FA_READ ) == FR_OK )
+	if (f_open_char(&fd, path, FA_READ|FA_OPEN_EXISTING) == FR_OK)
 	{
-		f_close( &fd );
+		f_close(&fd);
 		return true;
 	}
 	return false;
@@ -3360,7 +3365,7 @@ s32 Check_Cheats()
 #ifdef CHEATS
 	if( ConfigGetConfig(NIN_CFG_CHEAT_PATH) )
 	{
-		char *cpath = ConfigGetCheatPath();
+		const char *cpath = ConfigGetCheatPath();
 		if (cpath[0] != 0)
 		{
 			if( fileExist(cpath) )
@@ -3370,14 +3375,18 @@ s32 Check_Cheats()
 			}
 		}
 	}
+
 	u32 i;
-	char* DiscName = ConfigGetGamePath();
+	const char* DiscName = ConfigGetGamePath();
 	//search the string backwards for '/'
-	for( i=strlen(DiscName); i > 0; --i )
+	for (i = strlen(DiscName); i > 0; --i)
+	{
 		if( DiscName[i] == '/' )
 			break;
+	}
 	i++;
 	memcpy(cheatPath, DiscName, i);
+
 	//new version paths
 	_sprintf(cheatPath+i, "game.gct");
 	if( fileExist(cheatPath) )