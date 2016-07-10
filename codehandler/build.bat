9fbeac82894e54ea5fad4aa86d7bbe214d3568d5
David Korth <gerbilsoft@gerbilsoft.com>
2016-07-09 21:42:25 -0400

    Fix codehandler builds on Linux and improve it across the board.
    Broken due to commit 67fa83c8bd72fddb44214caec5782b84617a8e76 in the
    recent rebase. (r414)
    
    Changes:
    - Marked codehandler/build.sh as executable.
    - Create the .h files in the codehandler/ directory.
    - kernel: Add codehandler/ to the include path.
    - .gitignore: Ignore generated .h files in codehandler/.

--- codehandler/build.bat
+++ codehandler/build.bat
@@ -4,17 +4,21 @@ setlocal
 set devpath=%DEVKITPPC:/=\%\
 set devpath=%devpath:~1,1%:%devpath:~2%
 
-echo codehandler.s
-%devpath%\bin\powerpc-eabi-gcc -nostartfiles -nodefaultlibs -Wl,-Ttext,0x80001000 -o ../kernel/bin2h/codehandler.bin codehandler.s
-echo codehandleronly.s
-%devpath%\bin\powerpc-eabi-gcc -nostartfiles -nodefaultlibs -Wl,-Ttext,0x80001000 -o ../kernel/bin2h/codehandleronly.bin codehandleronly.s
-cd ../kernel/bin2h
+echo  CC          codehandler.s
+%devpath%\bin\powerpc-eabi-gcc -nostartfiles -nodefaultlibs -Wl,-Ttext,0x80001000 -o codehandler.bin codehandler.s
+echo  CC          codehandleronly.s
+%devpath%\bin\powerpc-eabi-gcc -nostartfiles -nodefaultlibs -Wl,-Ttext,0x80001000 -o codehandleronly.bin codehandleronly.s
+echo  STRIP       codehandler.bin
 %devpath%\bin\powerpc-eabi-strip --strip-debug --strip-all --discard-all -F elf32-powerpc codehandler.bin
-%devpath%\bin\powerpc-eabi-objcopy -I elf32-powerpc -O binary codehandler.bin
+echo  STRIP       codehandleronly.bin
 %devpath%\bin\powerpc-eabi-strip --strip-debug --strip-all --discard-all -F elf32-powerpc codehandleronly.bin
+echo  OBJCOPY     codehandler.bin
+%devpath%\bin\powerpc-eabi-objcopy -I elf32-powerpc -O binary codehandler.bin
+echo  OBJCOPY     codehandleronly.bin
 %devpath%\bin\powerpc-eabi-objcopy -I elf32-powerpc -O binary codehandleronly.bin
-bin2h codehandler.bin
-bin2h codehandleronly.bin
+echo  BIN2H       codehandler.h
+..\kernel\bin2h\bin2h.exe codehandler.bin
+echo  BIN2H       codehandleronly.h
+..\kernel\bin2h\bin2h.exe codehandleronly.bin
+echo  RM          codehandler.bin codehandleronly.bin
 del /f codehandler.bin codehandleronly.bin
-move /Y codehandler.h .. >nul
-move /Y codehandleronly.h .. >nul
--- codehandler/build.sh
+++ codehandler/build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo "codehandler.s"
-$DEVKITPPC/bin/powerpc-eabi-gcc -nostartfiles -nodefaultlibs -Wl,-Ttext,0x80001000 -o ../kernel/bin2h/codehandler.bin codehandler.s
-echo "codehandleronly.s"
-$DEVKITPPC/bin/powerpc-eabi-gcc -nostartfiles -nodefaultlibs -Wl,-Ttext,0x80001000 -o ../kernel/bin2h/codehandleronly.bin codehandleronly.s
-cd ../kernel/bin2h
+echo " CC          codehandler.s"
+$DEVKITPPC/bin/powerpc-eabi-gcc -nostartfiles -nodefaultlibs -Wl,-Ttext,0x80001000 -o codehandler.bin codehandler.s
+echo " CC          codehandleronly.s"
+$DEVKITPPC/bin/powerpc-eabi-gcc -nostartfiles -nodefaultlibs -Wl,-Ttext,0x80001000 -o codehandleronly.bin codehandleronly.s
+echo " STRIP       codehandler.bin"
 $DEVKITPPC/bin/powerpc-eabi-strip --strip-debug --strip-all --discard-all -F elf32-powerpc codehandler.bin
-$DEVKITPPC/bin/powerpc-eabi-objcopy -I elf32-powerpc -O binary codehandler.bin
+echo " STRIP       codehandleronly.bin"
 $DEVKITPPC/bin/powerpc-eabi-strip --strip-debug --strip-all --discard-all -F elf32-powerpc codehandleronly.bin
+echo " OBJCOPY     codehandler.bin"
+$DEVKITPPC/bin/powerpc-eabi-objcopy -I elf32-powerpc -O binary codehandler.bin
+echo " OBJCOPY     codehandleronly.bin"
 $DEVKITPPC/bin/powerpc-eabi-objcopy -I elf32-powerpc -O binary codehandleronly.bin
-bin2h codehandler.bin
-bin2h codehandleronly.bin
+echo " BIN2H       codehandler.h"
+../kernel/bin2h/bin2h codehandler.bin
+echo " BIN2H       codehandleronly.h"
+../kernel/bin2h/bin2h codehandleronly.bin
+echo " RM          codehandler.bin codehandleronly.bin"
 rm -f codehandler.bin codehandleronly.bin
-mv -f codehandler.h ..
-mv -f codehandleronly.h ..
