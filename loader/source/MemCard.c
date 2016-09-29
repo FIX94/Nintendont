54de988b5005bf502ea516a2a9eda79214b04b92
David Korth <gerbilsoft@gerbilsoft.com>
2016-09-29 00:37:43 -0400

    [loader] MemCard.c: Optimized memory card formatting.
    Use a struct for the header and direct pointer access for the directory
    and block tables. The memory card data is now allocated using memalign(),
    so all of the fields are guaranteed to be aligned correctly.
    
    TODO: Set up header->reserved1[] and header->formatTime correctly
    instead of just setting them to 0?
    
    (cherry picked from commit e35b94949ea4168f5fa1939810be1d7b05ac22ac)
    (with minor build and formatting fixes)

--- loader/source/MemCard.c
+++ loader/source/MemCard.c
@@ -20,19 +20,37 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "exi.h"
 #include "ff_utf8.h"
 
-/* Same Method as SRAM Checksum in Kernel */
-static void doChecksum(u16 *buffer, u32 size, u16 *c1, u16 *c2)
+// Memory card header.
+typedef struct __attribute__ ((packed)) _card_header
 {
-	u32 i;
-	u32 len = size / sizeof(u16);
+	u8 reserved1[12];
+	u64 formatTime;		// Format time. (OSTime value; 1 tick == 1/40,500,000 sec)
+	u32 sramBias;		// SRAM bias at time of format.
+	u32 sramLang;		// SRAM language.
+	u32 reserved2;
+
+	u16 device_id;		// 0 if formatted in slot A; 1 if formatted in slot B
+	u16 size;		// size of card, in Mbits
+	u16 encoding;		// 0 == cp1252; 1 == Shift-JIS
+
+	u8 reserved3[0x1D6];	// Should be 0xFF.
+	u16 chksum1;		// Checksum.
+	u16 chksum2;		// Inverted checksum.
+} card_header;
+
+
+/* Same Method as SRAM Checksum in Kernel */
+static void doChecksum(const u16 *buffer, u32 size, u16 *c1, u16 *c2)
+{
 	*c1 = 0; *c2 = 0;
-	for(i = 0; i < len; ++i)
+	u32 i;
+	for (i = size/sizeof(u16); i > 0; i--, buffer++)
 	{
-		*c1 += buffer[i];
-		*c2 += buffer[i] ^ 0xFFFF;
+		*c1 += *buffer;
+		*c2 += *buffer ^ 0xFFFF;
 	}
-	if(*c1 == 0xFFFF) *c1 = 0;
-	if(*c2 == 0xFFFF) *c2 = 0;
+	if (*c1 == 0xFFFF) *c1 = 0;
+	if (*c2 == 0xFFFF) *c2 = 0;
 }
 
 /**
@@ -49,56 +67,52 @@ bool GenerateMemCard(const char *MemCard)
 	if (f_open_char(&f, MemCard, FA_WRITE|FA_CREATE_NEW) != FR_OK)
 		return false;
 
-	// FIXME FOR LATER OPTIMIZATION: Use aligned malloc; then eliminate some memcpys.
 	//Get memory to format
-	u8 *MemcardBase = malloc(MEM_CARD_SIZE(ncfg->MemCardBlocks));
+	u8 *MemcardBase = memalign(32, MEM_CARD_SIZE(ncfg->MemCardBlocks));
 	memset(MemcardBase, 0, MEM_CARD_SIZE(ncfg->MemCardBlocks));
 	//Fill Header and Dir Memory with 0xFF
 	memset(MemcardBase, 0xFF, 0x6000);
+
+	// Header block.
+	card_header *header = (card_header*)MemcardBase;
 	//Normally this area contains a checksum which is built via SRAM and formatting time, you can
 	//skip that though because if the formatting time is 0 the resulting checksum is also 0 ;)
-	memset(MemcardBase, 0, 0x14);
+	memset(header->reserved1, 0, sizeof(header->reserved1));
+	header->formatTime = 0;		// TODO: Actually set this?
 	//From SRAM as defined in PatchCodes.h
-	u32 TmpU32 = 0x17CA2A85;
-	memcpy(MemcardBase + 0x14, &TmpU32, 4);
+	header->sramBias = 0x17CA2A85;
 	//Use current language for SRAM language
-	TmpU32 = ncfg->Language;
-	memcpy(MemcardBase + 0x18, &TmpU32, 4);
+	header->sramLang = ncfg->Language;
 	//Memory Card File Mode
-	TmpU32 = ((ncfg->GameID & 0xFF) == 'J' ? 2 : 0);
-	memcpy(MemcardBase + 0x1C, &TmpU32, 4);
+	header->reserved2 = ((ncfg->GameID & 0xFF) == 'J' ? 2 : 0);
+	//Assuming slot A.
+	header->device_id = 0;
 	//Memory Card size in MBits total
-	TmpU32 = MEM_CARD_SIZE(ncfg->MemCardBlocks) >> 17;
-	memcpy(MemcardBase + 0x20, &TmpU32, 4);
-	//Memory Card File Mode
-	u16 TmpShort = (ncfg->GameID & 0xFF) == 'J';
-	memcpy(MemcardBase + 0x24, &TmpShort, 2);
+	header->size = MEM_CARD_SIZE(ncfg->MemCardBlocks) >> 17;
+	//Memory Card filename encoding
+	header->encoding = ((ncfg->GameID & 0xFF) == 'J');
 	//Generate Header Checksum
-	doChecksum((u16*)(MemcardBase),0x1FC,(u16*)(MemcardBase+0x1FC),(u16*)(MemcardBase+0x1FE));
+	doChecksum((u16*)header, 0x1FC, &header->chksum1, &header->chksum2);
+
 	//Set Dir Update Counters
-	TmpShort = 0;
-	memcpy(MemcardBase + 0x3FFA, &TmpShort, 2);
-	TmpShort = 1;
-	memcpy(MemcardBase + 0x5FFA, &TmpShort, 2);
+	*((u16*)&MemcardBase[0x3FFA]) = 0;
+	*((u16*)&MemcardBase[0x5FFA]) = 1;
 	//Generate Dir Checksums
-	doChecksum((u16*)(MemcardBase+0x2000),0x1FFC,(u16*)(MemcardBase+0x3FFC),(u16*)(MemcardBase+0x3FFE));
-	doChecksum((u16*)(MemcardBase+0x4000),0x1FFC,(u16*)(MemcardBase+0x5FFC),(u16*)(MemcardBase+0x5FFE));
+	doChecksum((u16*)&MemcardBase[0x2000], 0x1FFC, (u16*)&MemcardBase[0x3FFC], (u16*)&MemcardBase[0x3FFE]);
+	doChecksum((u16*)&MemcardBase[0x4000], 0x1FFC, (u16*)&MemcardBase[0x5FFC], (u16*)&MemcardBase[0x5FFE]);
+
 	//Set Block Update Counters
-	TmpShort = 0;
-	memcpy(MemcardBase + 0x6004, &TmpShort, 2);
-	TmpShort = 1;
-	memcpy(MemcardBase + 0x8004, &TmpShort, 2);
+	*((u16*)&MemcardBase[0x6004]) = 0;
+	*((u16*)&MemcardBase[0x8004]) = 1;
 	//Set Free Blocks
-	TmpShort = MEM_CARD_BLOCKS(ncfg->MemCardBlocks);
-	memcpy(MemcardBase + 0x6006, &TmpShort, 2);
-	memcpy(MemcardBase + 0x8006, &TmpShort, 2);
+	*((u16*)&MemcardBase[0x6006]) = MEM_CARD_BLOCKS(ncfg->MemCardBlocks);
+	*((u16*)&MemcardBase[0x8006]) = MEM_CARD_BLOCKS(ncfg->MemCardBlocks);
 	//Set last allocated Block
-	TmpShort = 4;
-	memcpy(MemcardBase + 0x6008, &TmpShort, 2);
-	memcpy(MemcardBase + 0x8008, &TmpShort, 2);
+	*((u16*)&MemcardBase[0x6008]) = 4;
+	*((u16*)&MemcardBase[0x8008]) = 4;
 	//Generate Block Checksums
-	doChecksum((u16*)(MemcardBase+0x6004),0x1FFC,(u16*)(MemcardBase+0x6000),(u16*)(MemcardBase+0x6002));
-	doChecksum((u16*)(MemcardBase+0x8004),0x1FFC,(u16*)(MemcardBase+0x8000),(u16*)(MemcardBase+0x8002));
+	doChecksum((u16*)&MemcardBase[0x6004], 0x1FFC, (u16*)&MemcardBase[0x6000], (u16*)&MemcardBase[0x6002]);
+	doChecksum((u16*)&MemcardBase[0x8004], 0x1FFC, (u16*)&MemcardBase[0x8000], (u16*)&MemcardBase[0x8002]);
 
 	//Write it into a file
 	UINT wrote;
