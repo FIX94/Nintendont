FIX94/Nintendont

commit 02483dba4600a1a0dd845ccd323753e649e4aea0

[loader] main.c: Split multi-game disc selection into a separate function.
New function CheckForMultiGame() checks if this is a multi-game disc.
If not, it returns immediately with an ISOShift value of 0. Otherwise,
it prompts the user to select one of the games, and then returns that
game's offset.

Added a prompt to select a game from the multi-game disc.

TODO: Use menu.c's menu code.

Split multi-game disc checking into a new function in global.c,
IsMultiGameDisc(). Updated main.c and menu.c to use this function.

loader/source/main.c
@@ -201,6 +201,180 @@ void changeToDefaultDrive()
 	f_chdir_char("/");
 }
 
+/**
+ * Check for a multi-game image.
+ * @param CurDICMD DI command. (0 == disc image, DIP_CMD_NORMAL == GameCube disc, DIP_CMD_DVDR == DVD-R)
+ * @return ISO Shift. (0 for non-multi; byte offset for multi.)
+ */
+static int CheckForMultiGame(u32 CurDICMD)
+{
+	// Re-read the disc header to get the full ID6.
+	u8 *MultiHdr = memalign(32, 0x800);
+
+	FIL f;
+	UINT read;
+	FRESULT fres = FR_DISK_ERR;
+
+	if(CurDICMD)
+	{
+		ReadRealDisc(MultiHdr, 0, 0x800, CurDICMD);
+	}
+	else if (IsSupportedFileExt(ncfg->GamePath))
+	{
+		char GamePath[260];
+		snprintf(GamePath, sizeof(GamePath), "%s:%s", GetRootDevice(), ncfg->GamePath);
+		fres = f_open_char(&f, GamePath, FA_READ|FA_OPEN_EXISTING);
+		if (fres != FR_OK)
+		{
+			// Error opening the file.
+			free(MultiHdr);
+			return 0;
+		}
+
+		f_read(&f, MultiHdr, 0x800, &read);
+		if (read != 0x800)
+		{
+			// Error reading from the file.
+			f_close(&f);
+			free(MultiHdr);
+			return 0;
+		}
+
+		// NOTE: Not checking for CISO format here.
+	}
+
+	if (!IsMultiGameDisc((const char*)MultiHdr))
+	{
+		// Not a multi-game disc.
+		f_close(&f);
+		free(MultiHdr);
+		return 0;
+	}
+
+	// Up to 15 games are supported.
+	// In theory, the format supports up to 48 games, but
+	// you'll run into the DVD size limit way before you
+	// ever reach that limit.
+	u32 i = 0;
+	u32 gamecount = 0;
+	u32 Offsets[15];
+	gameinfo gi[15];
+
+	u8 *GameHdr = memalign(32, 0x800);
+	// GCOPDV(D9) uses Wii-style 34-bit shifted addresses.
+	// FIXME: Needs 64-bit offsets.
+	const u32 *hdr32 = (const u32*)MultiHdr;
+	bool NeedShift = (hdr32[1] == 0x44564439);
+	for (i = 0x10; i < 0x40 && gamecount < 15; i++)
+	{
+		const u32 TmpOffset = hdr32[i];
+		if (TmpOffset > 0)
+		{
+			Offsets[gamecount] = NeedShift ? TmpOffset << 2 : TmpOffset;
+			if(CurDICMD)
+			{
+				ReadRealDisc(GameHdr, Offsets[gamecount], 0x800, CurDICMD);
+			}
+			else
+			{
+				// FIXME: CISO support.
+				f_lseek(&f, Offsets[gamecount]);
+				f_read(&f, GameHdr, 0x800, &read);
+			}
+
+			// Make sure the title in the header is NULL terminated.
+			GameHdr[0x20+65] = 0;
+
+			// TODO: titles.txt support?
+			memcpy(gi[gamecount].ID, GameHdr, 6);
+			gi[gamecount].DiscNumber = 0;
+			gi[gamecount].Flags = GIFLAG_NAME_ALLOC;
+			gi[gamecount].Name = strdup((char*)&GameHdr[0x20]);
+			gi[gamecount].Path = NULL;
+			gamecount++;
+		}
+	}
+
+	free(GameHdr);
+	free(MultiHdr);
+	if (!CurDICMD)
+	{
+		f_close(&f);
+	}
+
+	// TODO: Share code with menu.c.
+	bool redraw = true;
+	ClearScreen();
+	u32 PosX = 0;
+	u32 UpHeld = 0, DownHeld = 0;
+	while (true)
+	{
+		VIDEO_WaitVSync();
+		FPAD_Update();
+		if( FPAD_OK(0) )
+			break;
+
+		if( FPAD_Down(1) )
+		{
+			if(DownHeld == 0 || DownHeld > 10)
+			{
+				PosX++;
+				if(PosX == gamecount) PosX = 0;
+				redraw = true;
+			}
+			DownHeld++;
+		}
+		else
+		{
+			DownHeld = 0;
+		}
+
+		if( FPAD_Up(1) )
+		{
+			if(UpHeld == 0 || UpHeld > 10)
+			{
+				if(PosX == 0) PosX = gamecount;
+				PosX--;
+				redraw = true;
+			}
+			UpHeld++;
+		}
+		else
+		{
+			UpHeld = 0;
+		}
+
+		// TODO: Home = Go Back?
+
+		if( redraw )
+		{
+			PrintInfo();
+			static const int subheader_x = (640 - (40*10)) / 2;
+			PrintFormat(DEFAULT_SIZE, BLACK, subheader_x, MENU_POS_Y + 20*3,
+				    "Select a game from this multi-game disc:");
+			for (i = 0; i < gamecount; ++i)
+			{
+				PrintFormat(DEFAULT_SIZE, BLACK, MENU_POS_X, MENU_POS_Y + 20*4 + i * 20, "%50.50s [%.6s]%s", 
+					    gi[i].Name, gi[i].ID, i == PosX ? ARROW_LEFT : " " );
+			}
+			GRRLIB_Render();
+			Screenshot();
+			ClearScreen();
+			redraw = false;
+		}
+	}
+
+	// Free the allocated names.
+	for (i = 0; i < gamecount; ++i)
+	{
+		if (gi[i].Flags & GIFLAG_NAME_ALLOC)
+			free(gi[i].Name);
+	}
+
+	memcpy(&(ncfg->GameID), gi[PosX].ID, 4);
+	return Offsets[PosX];
+}
+
 int main(int argc, char **argv)
 {
 	// Exit after 10 seconds if there is an error
@@ -495,123 +669,8 @@ int main(int argc, char **argv)
 		}
 	}
 
-	// Check for multi-game disc images.
-	u32 ISOShift = 0;
-	if(memcmp(&(ncfg->GameID), "COBR", 4) == 0 || memcmp(&(ncfg->GameID), "GGCO", 4) == 0
-		|| memcmp(&(ncfg->GameID), "GCO", 3) == 0 || memcmp(&(ncfg->GameID), "RGCO", 4) == 0)
-	{
-		u32 i, j = 0;
-		u32 Offsets[15];
-		gameinfo gi[15];
-		u8 *MultiHdr = memalign(32, 0x800);
-
-		FIL f;
-		UINT read;
-		FRESULT fres = FR_DISK_ERR;
-
-		if(CurDICMD)
-		{
-			ReadRealDisc(MultiHdr, 0, 0x800, CurDICMD);
-		}
-		else if (IsSupportedFileExt(ncfg->GamePath))
-		{
-			char GamePath[260];
-			snprintf(GamePath, sizeof(GamePath), "%s:%s", GetRootDevice(), ncfg->GamePath);
-			fres = f_open_char(&f, GamePath, FA_READ|FA_OPEN_EXISTING);
-			if (fres == FR_OK)
-				f_read(&f, MultiHdr, 0x800, &read);
-		}
-
-		//Damn you COD for sharing this ID!
-		if (fres != FR_OK || (memcmp(MultiHdr, "GCO", 3) == 0 && memcmp(MultiHdr+4, "52", 3) == 0))
-		{
-			free(MultiHdr);
-			if (fres == FR_OK)
-				f_close(&f);
-		}
-		else
-		{
-			u8 *GameHdr = memalign(32, 0x800);
-			u32 NeedShift = (*(vu32*)(MultiHdr+4) == 0x44564439);
-			for(i = 0x40; i < 0x100; i += 4)
-			{
-				u32 TmpOffset = *(vu32*)(MultiHdr+i);
-				if(TmpOffset > 0)
-				{
-					Offsets[j] = NeedShift ? TmpOffset << 2 : TmpOffset;
-					if(CurDICMD)
-					{
-						ReadRealDisc(GameHdr, Offsets[j], 0x800, CurDICMD);
-					}
-					else
-					{
-						f_lseek(&f, Offsets[j]);
-						f_read(&f, GameHdr, 0x800, &read);
-					}
-
-					// TODO: titles.txt support?
-					// FIXME: This memory is never freed!
-					memcpy(gi[j].ID, GameHdr, 6);
-					gi[j].Name = strdup((char*)GameHdr+0x20);
-					j++;
-					if(j == 15) break;
-				}
-			}
-			free(GameHdr);
-			free(MultiHdr);
-			f_close(&f);
-
-			bool redraw = 1;
-			ClearScreen();
-			u32 PosX = 0;
-			u32 UpHeld = 0, DownHeld = 0;
-			while(1)
-			{
-				VIDEO_WaitVSync();
-				FPAD_Update();
-				if( FPAD_OK(0) )
-					break;
-				else if( FPAD_Down(1) )
-				{
-					if(DownHeld == 0 || DownHeld > 10)
-					{
-						PosX++;
-						if(PosX == j) PosX = 0;
-						redraw = true;
-					}
-					DownHeld++;
-				}
-				else
-					DownHeld = 0;
-				if( FPAD_Up(1) )
-				{
-					if(UpHeld == 0 || UpHeld > 10)
-					{
-						if(PosX == 0) PosX = j;
-						PosX--;
-						redraw = true;
-					}
-					UpHeld++;
-				}
-				else
-					UpHeld = 0;
-				if( redraw )
-				{
-					PrintInfo();
-					for( i=0; i < j; ++i )
-						PrintFormat(DEFAULT_SIZE, BLACK, MENU_POS_X, MENU_POS_Y + 20*4 + i * 20, "%50.50s [%.6s]%s", 
-							gi[i].Name, gi[i].ID, i == PosX ? ARROW_LEFT : " " );
-					GRRLIB_Render();
-					Screenshot();
-					ClearScreen();
-					redraw = false;
-				}
-			}
-			ISOShift = Offsets[PosX];
-			memcpy(&(ncfg->GameID), gi[PosX].ID, 4);
-		}
-	}
-//multi-iso game hack
+	// Check if this is a multi-game disc image.
+	u32 ISOShift = CheckForMultiGame(CurDICMD);
 	*(vu32*)0xD300300C = ISOShift;
 
 //Set Language

loader/include/global.h
@@ -195,6 +195,13 @@ void CloseDevices(void);
  */
 bool IsSupportedFileExt(const char *filename);
 
+/**
+ * Check if an ID6 is a known multi-game disc.
+ * @param id6 ID6. (must be 6 bytes)
+ * @return True if this is a known multi-game disc; false if not.
+ */
+bool IsMultiGameDisc(const char *id6);
+
 #endif
 
 // 78A94

loader/source/global.c
@@ -597,3 +597,33 @@ bool IsSupportedFileExt(const char *filename)
 	// File extension is NOT supported.
 	return false;
 }
+
+/**
+ * Check if an ID6 is a known multi-game disc.
+ * @param id6 ID6. (must be 6 bytes)
+ * @return True if this is a known multi-game disc; false if not.
+ */
+bool IsMultiGameDisc(const char *id6)
+{
+	// Reference: https://gbatemp.net/threads/wit-wiimms-iso-tools-gamecube-disc-support.251630/#post-3088119
+	if (!memcmp(id6, "GCO", 3) && id6[4]=='D' && id6[5]=='V')
+	{
+		// GCOSDV(D5) or GCOSDV(D9).
+		return true;
+	}
+
+	// Check for older IDs.
+	static const char multi_game_ids[3][8] = {"COBRAM", "GGCOSD", "RGCOSD"};
+	u32 i;
+	for (i = 0; i < 3; i++)
+	{
+		if (!memcmp(id6, multi_game_ids[i], 6))
+		{
+			// Found a multi-game disc.
+			return true;
+		}
+	}
+
+	// Not a multi-game disc.
+	return false;
+}

loader/source/menu.c
@@ -111,11 +111,10 @@ int compare_names(const void *a, const void *b)
  * @param gi		[out] gameinfo struct to store game information if the disc is valid.
  * @return True if the image is valid; false if not.
  */
-bool IsDiscImageValid(const char *filename, int discNumber, gameinfo *gi)
+static bool IsDiscImageValid(const char *filename, int discNumber, gameinfo *gi)
 {
 	// TODO: Handle FST format (sys/boot.bin).
 	u8 buf[0x100];		// Disc header.
-	int i;
 
 	FIL in;
 	if (f_open_char(&in, filename, FA_READ|FA_OPEN_EXISTING) != FR_OK)
@@ -183,26 +182,7 @@ bool IsDiscImageValid(const char *filename, int discNumber, gameinfo *gi)
 
 		// Check if this is a multi-game image.
 		// Reference: https://gbatemp.net/threads/wit-wiimms-iso-tools-gamecube-disc-support.251630/#post-3088119
-		bool is_multigame = false;
-		if (!memcmp(buf, "GCO", 3) && buf[4]=='D' && buf[5]=='V')
-		{
-			// GCOSDV(D5) or GCOSDV(D9).
-			is_multigame = true;
-		}
-		else
-		{
-			static const char multi_game_ids[3][8] = {"COBRAM", "GGCOSD", "RGCOSD"};
-			for (i = 0; i < 3; i++)
-			{
-				if (!memcmp(buf, multi_game_ids[i], 6))
-				{
-					// Found a multi-game disc.
-					is_multigame = true;
-					break;
-				}
-			}
-		}
-
+		const bool is_multigame = IsMultiGameDisc((const char*)buf);
 		if (is_multigame)
 		{
 			if (gi->Flags == GIFLAG_FORMAT_CISO)

commit 3a340e1c0b07c95bedc8c569ad726dd1854acaf5

[loader] Fixed a crash when selecting a game in Extracted FST format.
CheckForMultiGame() wasn't returning in this case, so it attempted
to read from a FatFS file that was never opened, resulting in
massive failure.

This fixes a regression from commit 02483dba4600a1a0dd845ccd323753e649e4aea0.
([loader] main.c: Split multi-game disc selection into a separate function.)

loader/source/main.c
@@ -242,6 +242,12 @@ static int CheckForMultiGame(u32 CurDICMD)
 
 		// NOTE: Not checking for CISO format here.
 	}
+	else
+	{
+		// Extracted FST format.
+		// Multi-game isn't supported.
+		return 0;
+	}
 
 	if (!IsMultiGameDisc((const char*)MultiHdr))
 	{