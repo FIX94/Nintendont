b7f4d5688fbdbb2c96e71679d5cd379847272c34
David Korth <gerbilsoft@gerbilsoft.com>
2016-10-02 14:58:30 -0400

    Nintendont v4.422-CISO-test4 is now ready for testing.

--- common/include/NintendontVersion.h
+++ common/include/NintendontVersion.h
@@ -12,6 +12,6 @@
 
 // "Special" version.
 // This should only be set in custom builds, i.e. not mainline.
-#define NIN_SPECIAL_VERSION			"-CISO-test3"
+#define NIN_SPECIAL_VERSION			"-CISO-test4"
 
 #endif
