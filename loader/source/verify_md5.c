ea0605446da232039702a08327752fa04b154c37
David Korth <gerbilsoft@gerbilsoft.com>
2016-11-05 01:46:49 -0400

    [loader] verify_md5.c: Preparations for loading an MD5 database file and looking up the MD5.
    If gcn_md5.txt isn't found, a warning is displayed on the bottom of the
    screen when it starts to calculate the MD5.
    
    TODO: Actually load gcn_md5.txt and then search for the MD5.

--- loader/source/verify_md5.c
+++ loader/source/verify_md5.c
@@ -28,9 +28,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include <stdio.h>
 #include <sys/time.h>
+#include <unistd.h>
 #include <ogc/lwp_watchdog.h>
 
 #include "ff_utf8.h"
+
+extern char launch_dir[MAXPATHLEN];
 
 /**
  * Get the time of day with correct microseconds.
@@ -59,6 +62,95 @@ static int gettimeofday_rvlfix(struct timeval *tv, struct timezone *tz)
 }
 
 /**
+ * Convert MD5 bytes to a string.
+ * @param md5_str Output string. (Must be 33 bytes.)
+ * @param digest MD5 digest. (Must be 16 bytes.)
+ */
+static void md5_to_str(char md5_str[33], const md5_byte_t digest[16])
+{
+	// Convert the MD5 to a string.
+	static const char hex_lookup[16] = {
+		'0','1','2','3','4','5','6','7',
+		'8','9','A','B','C','D','E','F'
+	};
+	int i;
+	for (i = 0; i < 16; i++) {
+		md5_str[(i*2)+0] = hex_lookup[digest[i] >> 4];
+		md5_str[(i*2)+1] = hex_lookup[digest[i] & 0x0F];
+	}
+	md5_str[32] = 0;
+}
+
+#define STR_X(len) ((640 - ((len)*10)) / 2)
+#define STR_CONST_X(str) STR_X(sizeof(str)-1)
+#define STR_PTR_X(str) STR_X(strlen(str))
+
+static const char db_not_found[] = "WARNING: gcn_md5.txt not found; cannot verify MD5s.";
+
+/**
+ * Show a status update.
+ * NOTE: Caller should print other lines and then render the text.
+ * @param is_md5_found True if gcn_md5.txt was found.
+ * @param gamepath Game path.
+ * @param total_read Total number of bytes read so far.
+ * @param total_size Total size of the disc image.
+ */
+static void ShowStatusUpdate(bool is_md5_found, const char *gamepath, u32 total_read, u32 total_size)
+{
+	// Status update.
+	ClearScreen();
+	PrintInfo();
+	PrintFormat(DEFAULT_SIZE, BLACK, MENU_POS_X + 430, MENU_POS_Y + 20*2, "B   : Cancel");
+
+	static const char md5_calculating[] = "Calculating MD5...";
+	PrintFormat(DEFAULT_SIZE, BLACK, STR_CONST_X(md5_calculating), 232-40, md5_calculating);
+	PrintFormat(DEFAULT_SIZE, BLACK, STR_PTR_X(gamepath), 232, "%s", gamepath);
+
+	// Print the status, in MiB.
+	char status_msg[128];
+	int len = snprintf(status_msg, sizeof(status_msg),
+		"%u of %u MiB processed",
+		total_read / (1024*1024), total_size / (1024*1024));
+	PrintFormat(DEFAULT_SIZE, BLACK, STR_X(len), 232+40, "%s", status_msg);
+
+	if (!is_md5_found)
+	{
+		PrintFormat(DEFAULT_SIZE, MAROON, STR_CONST_X(db_not_found), 232+180, db_not_found);
+	}
+
+	// Caller should print other lines and then render the text.
+}
+/**
+ * Show the "Finish" screen.
+ * NOTE: Caller should print other lines and then render the text.
+ * @param is_md5_found True if gcn_md5.txt was found.
+ * @param time_diff Time taken to calculate the MD5.
+ * @param gamepath Game path.
+ * @param md5_str MD5 as a string.
+ */
+static void ShowFinishScreen(bool is_md5_found, float time_diff, const char *gamepath, const char *md5_str)
+{
+	char status_msg[128];
+
+	ClearScreen();
+	PrintInfo();
+	int len = snprintf(status_msg, sizeof(status_msg), "MD5 calculated in %0.1f seconds.", time_diff);
+	PrintFormat(DEFAULT_SIZE, BLACK, STR_X(len), 232-40, status_msg);
+	PrintFormat(DEFAULT_SIZE, BLACK, STR_PTR_X(gamepath), 232, "%s", gamepath);
+
+	// Print the MD5.
+	len = snprintf(status_msg, sizeof(status_msg), "MD5: %s", md5_str);
+	PrintFormat(DEFAULT_SIZE, BLACK, STR_X(len), 232+40, "%s", status_msg);
+
+	if (!is_md5_found)
+	{
+		PrintFormat(DEFAULT_SIZE, MAROON, STR_CONST_X(db_not_found), 232+180, db_not_found);
+	}
+
+	// Caller should print other lines and then render the text.
+}
+
+/**
  * Verify a game's MD5.
  * @param gameinfo Game to verify.
  */
@@ -72,9 +164,6 @@ void VerifyMD5(const gameinfo *gi)
 	PrintInfo();
 
 	// Initial status.
-	#define STR_X(len) ((640 - ((len)*10)) / 2)
-	#define STR_CONST_X(str) STR_X(sizeof(str)-1)
-	#define STR_PTR_X(str) STR_X(strlen(str))
 	static const char opening_str[] = "Opening disc image for verification:";
 	PrintFormat(DEFAULT_SIZE, BLACK, STR_CONST_X(opening_str), 232-40, opening_str);
 	PrintFormat(DEFAULT_SIZE, BLACK, STR_PTR_X(gi->Path), 232, "%s", gi->Path);
@@ -120,9 +209,19 @@ void VerifyMD5(const gameinfo *gi)
 	}
 #endif /* _USE_FASTSEEK */
 
-	// snprintf() buffer for the status message.
-	char status_msg[128];
-	int len;
+	// Determine the gcn_md5.txt path.
+	// If loaded from network, launch_dir[] is empty,
+	// so use /apps/Nintendont/ as a fallback.
+	char filepath[MAXPATHLEN];
+	snprintf(filepath, sizeof(filepath), "%sgcn_md5.txt",
+		 launch_dir[0] != 0 ? launch_dir : "/apps/Nintendont/");
+	FIL f_md5;
+	bool is_md5_found = false;
+	if (f_open_char(&f_md5, filepath, FA_READ|FA_OPEN_EXISTING) == FR_OK)
+	{
+		// Found the MD5 database file.
+		is_md5_found = true;
+	}
 
 	// Read 64 KB at a time.
 	static const u32 buf_sz = 1024*1024;
@@ -138,7 +237,7 @@ void VerifyMD5(const gameinfo *gi)
 	// Start time.
 	struct timeval tv;
 	gettimeofday_rvlfix(&tv, NULL);
-	float time_start = tv.tv_sec + ((float)tv.tv_usec / 1000000.0f);
+	const float time_start = tv.tv_sec + ((float)tv.tv_usec / 1000000.0f);
 
 	u32 total_read = 0;
 	u32 total_read_mb = 0;
@@ -173,22 +272,7 @@ void VerifyMD5(const gameinfo *gi)
 		if (new_total_read_mb != total_read_mb)
 		{
 			new_total_read_mb = total_read_mb;
-
-			// Status update.
-			ClearScreen();
-			PrintInfo();
-			PrintFormat(DEFAULT_SIZE, BLACK, MENU_POS_X + 430, MENU_POS_Y + 20*2, "B   : Cancel");
-
-			static const char md5_calculating[] = "Calculating MD5...";
-			PrintFormat(DEFAULT_SIZE, BLACK, STR_CONST_X(md5_calculating), 232-40, md5_calculating);
-			PrintFormat(DEFAULT_SIZE, BLACK, STR_PTR_X(gi->Path), 232, "%s", gi->Path);
-
-			// Print the status, in MiB.
-			len = snprintf(status_msg, sizeof(status_msg),
-				"%u of %u MiB processed",
-				total_read / (1024*1024), total_size / (1024*1024));
-			PrintFormat(DEFAULT_SIZE, BLACK, STR_X(len), 232+40, "%s", status_msg);
-
+			ShowStatusUpdate(is_md5_found, gi->Path, total_read, total_size);
 			// Render the text.
 			GRRLIB_Render();
 			ClearScreen();
@@ -204,6 +288,10 @@ void VerifyMD5(const gameinfo *gi)
 	if (cancel)
 	{
 		// User cancelled the operation.
+		if (is_md5_found)
+		{
+			f_close(&f_md5);
+		}
 		return;
 	}
 
@@ -212,38 +300,36 @@ void VerifyMD5(const gameinfo *gi)
 
 	// End time.
 	gettimeofday_rvlfix(&tv, NULL);
-	float time_end = tv.tv_sec + ((float)tv.tv_usec / 1000000.0f);
+	const float time_end = tv.tv_sec + ((float)tv.tv_usec / 1000000.0f);
+	const float time_diff = time_end - time_start;
 
 	// Finished processing the MD5.
 	// TODO: Verify against a list of MD5s.
-	ClearScreen();
-	PrintInfo();
-	len = snprintf(status_msg, sizeof(status_msg), "MD5 calculated in %0.1f seconds.", time_end - time_start);
-	static const char press_a_btn[] = "Press the A button to continue.";
-	PrintFormat(DEFAULT_SIZE, BLACK, STR_X(len), 232-40, status_msg);
-	PrintFormat(DEFAULT_SIZE, BLACK, STR_PTR_X(gi->Path), 232, "%s", gi->Path);
-	PrintFormat(DEFAULT_SIZE, BLACK, STR_CONST_X(press_a_btn), 232+80, press_a_btn);
-
-	// Convert the MD5 to a string.
-	static const char hex_lookup[16] = {
-		'0','1','2','3','4','5','6','7',
-		'8','9','A','B','C','D','E','F'
-	};
 	char md5_str[33];
-	int i;
-	for (i = 0; i < 16; i++) {
-		md5_str[(i*2)+0] = hex_lookup[digest[i] >> 4];
-		md5_str[(i*2)+1] = hex_lookup[digest[i] & 0x0F];
+	md5_to_str(md5_str, digest);
+	ShowFinishScreen(is_md5_found, time_diff, gi->Path, md5_str);
+
+	static const char press_a_button[] = "Press the A button to continue...";
+
+	if (is_md5_found)
+	{
+		// Check the MD5 database.
+		static const char checking_db[] = "Checking MD5 database...";
+		PrintFormat(DEFAULT_SIZE, BLACK, STR_CONST_X(checking_db), 232+80, checking_db);
+		// Render the text.
+		GRRLIB_Render();
+		ClearScreen();
+
+		// TODO: Verify the MD5.
+		f_close(&f_md5);
 	}
-	md5_str[32] = 0;
-
-	// Print the MD5.
-	len = snprintf(status_msg, sizeof(status_msg), "MD5: %s", md5_str);
-	PrintFormat(DEFAULT_SIZE, BLACK, STR_X(len), 232+40, "%s", status_msg);
-
-	// Render the text.
-	GRRLIB_Render();
-	ClearScreen();
+	else
+	{
+		PrintFormat(DEFAULT_SIZE, BLACK, STR_CONST_X(press_a_button), 232+80, press_a_button);
+		// Render the text.
+		GRRLIB_Render();
+		ClearScreen();
+	}
 
 	// Wait for the user to press the A button.
 	while (1)
