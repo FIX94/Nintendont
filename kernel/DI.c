FIX94/Nintendont

commit 6f67b91031a27ab08b91f3c45a6a335849037d2e

Initial attempt at supporting multiple extensions for 2-disc games.
DIinit(): Check for the various disc image filenames on startup.
If found, save pointers to them. These string pointers replace
MultipleDiscs; if NULL, single-disc mode is used.

DIChangeDisc(): Use the saved filename pointers to create the 2-disc
filenames.

[loader] menu.c: Allow all combinations to be used for disc2.

kernel/DI.c
@@ -18,6 +18,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
+
+// strcasecmp()
+#include <strings.h>
+
 #include "global.h"
 #include "DI.h"
 #include "RealDI.h"
@@ -51,7 +55,6 @@ u8 *DI_MessageHeap = NULL;
 bool DI_IRQ = false;
 u32 DI_Thread = 0;
 s32 DI_Handle = -1;
-bool MultipleDiscs = true;
 u32 ISOShift = 0;
 static u32 Streaming = 0; //internal
 extern u32 StreamSize, StreamStart, StreamCurrent, StreamEndOffset;
@@ -83,6 +86,17 @@ u32 GCAMKeyC;
 u8 *MediaBuffer;
 u8 *NetworkCMDBuffer;
 u8 *DIMMMemory = (u8*)0x12B80000;
+
+// Multi-disc filenames.
+static const char disc_filenames[8][16] = {
+	"game.ciso", "game.cso", "game.gcm", "game.iso",
+	"disc2.ciso", "disc2.cso", "disc2.gcm", "disc2.iso"
+};
+
+// Filename portions for 2-disc mode.
+// Points to entries in disc_filenames.
+// NOTE: If either is NULL, assume single-disc mode.
+static const char *DI_2disc_filenames[2] = {NULL, NULL};
 
 void DIRegister(void)
 {
@@ -147,24 +161,49 @@ void DIinit( bool FirstTime )
 	{
 		if(RealDiscCMD == 0)
 		{
-			// Move this to ISO.c?
-			u32 i;
+			// Check if this is a 2-disc game.
+			u32 i, slash_pos;
 			char TempDiscName[256];
 			_sprintf(TempDiscName, "%s", ConfigGetGamePath());
 
 			//search the string backwards for '/'
-			for( i=strlen(TempDiscName); i > 0; --i )
-				if( TempDiscName[i] == '/' )
+			for (slash_pos = strlen(TempDiscName); slash_pos > 0; --slash_pos)
+			{
+				if (TempDiscName[slash_pos] == '/')
 					break;
-			i++;
+			}
+			slash_pos++;
 
-			_sprintf(TempDiscName+i, "disc2.iso");
-			FIL ExistsFile;
-			s32 ret = f_open_char(&ExistsFile, TempDiscName, FA_READ);
-			if (ret != FR_OK)
-				MultipleDiscs = false;
-			else
-				f_close(&ExistsFile);
+			// First, make sure the disc's filename is game.(ciso|cso|gcm|iso).
+			DI_2disc_filenames[0] = NULL;
+			DI_2disc_filenames[1] = NULL;
+			for (i = 0; i < 4; i++)
+			{
+				if (!strcasecmp(TempDiscName+slash_pos, disc_filenames[i]))
+				{
+					// This is game.(ciso|cso|gcm|iso).
+					DI_2disc_filenames[0] = disc_filenames[i];
+					break;
+				}
+			}
+
+			if (DI_2disc_filenames[0] != NULL)
+			{
+				// Check for the disc2 file.
+				for (i = 4; i < 8; i++)
+				{
+					_sprintf(TempDiscName+slash_pos, disc_filenames[i]);
+					FIL ExistsFile;
+					s32 ret = f_open_char(&ExistsFile, TempDiscName, FA_READ);
+					if (ret == FR_OK)
+					{
+						// Found the disc image.
+						f_close(&ExistsFile);
+						DI_2disc_filenames[1] = disc_filenames[i];
+						break;
+					}
+				}
+			}
 		}
 		else
 		{
@@ -204,23 +243,26 @@ void DISetDIMMVersion( u32 Version )
 }
 bool DIChangeDisc( u32 DiscNumber )
 {
-	if (!MultipleDiscs)
+	// Don't do anything if multi-disc mode isn't enabled.
+	if (!DI_2disc_filenames[0] ||
+	    !DI_2disc_filenames[1] ||
+	    DiscNumber > 1)
+	{
 		return false;
+	}
 
-	u32 i;
+	u32 slash_pos;
 	char* DiscName = ConfigGetGamePath();
 
 	//search the string backwards for '/'
-	for( i=strlen(DiscName); i > 0; --i )
-		if( DiscName[i] == '/' )
+	for (slash_pos = strlen(DiscName); slash_pos > 0; --slash_pos)
+	{
+		if (DiscName[slash_pos] == '/')
 			break;
-	i++;
+	}
+	slash_pos++;
 
-	if( DiscNumber == 0 )
-		_sprintf( DiscName+i, "game.iso" );
-	else
-		_sprintf( DiscName+i, "disc2.iso" );
-
+	_sprintf(DiscName+slash_pos, DI_2disc_filenames[DiscNumber]);
 	dbgprintf("New Gamepath:\"%s\"\r\n", DiscName );
 	DIinit(false);
 	return true;

kernel/ISO.c
@@ -61,14 +61,14 @@ typedef struct _CISO_t {
 // CISO: Block map.
 // Supports files up to 2 GB when using 2 MB blocks.
 static uint16_t ciso_block_map[CISO_MAP_SIZE];
-static int is_ciso = 0;	// Set to 1 for CISO mode.
+static bool ISO_IsCISO = false;	// Set to 1 for CISO mode.
 
 static inline void ISOReadDirect(void *Buffer, u32 Length, u32 Offset)
 {
 	if(ISOFileOpen == 0)
 		return;
 
-	if (!is_ciso)
+	if (!ISO_IsCISO)
 	{
 		// Standard ISO/GCM file.
 		if(LastOffset != Offset)
@@ -226,6 +226,7 @@ bool ISOInit()
 	/* Setup direct reader */
 	ISOFileOpen = 1;
 	LastOffset = UINT_MAX;
+	ISO_IsCISO = false;
 
 	/* Check for CISO format. */
 	CISO_t *tmp_ciso = (CISO_t*)CACHE_START;
@@ -269,7 +270,7 @@ bool ISOInit()
 			}
 
 			// Enable CISO mode.
-			is_ciso = 1;
+			ISO_IsCISO = true;
 		}
 	}
 
@@ -302,6 +303,7 @@ void ISOClose()
 #endif /* _USE_FASTSEEK */
 	}
 	ISOFileOpen = 0;
+	ISO_IsCISO = false;
 }
 
 void ISOSetupCache()

loader/source/menu.c
@@ -292,12 +292,16 @@ static DevState LoadGameList(gameinfo *gi, u32 sz, u32 *pGameCount)
 		 * ISO/GCM format:
 		 * - /games/GAMEID/game.gcm
 		 * - /games/GAMEID/game.iso
-		 * - /games/GAMEID/disc2.iso [.gcm not supported yet]
+		 * - /games/GAMEID/disc2.gcm
+		 * - /games/GAMEID/disc2.iso
 		 * - /games/[anything].gcm
 		 * - /games/[anything].iso
 		 *
 		 * CISO format:
 		 * - /games/GAMEID/game.ciso
+		 * - /games/GAMEID/game.cso
+		 * - /games/GAMEID/disc2.ciso
+		 * - /games/GAMEID/disc2.cso
 		 * - /games/[anything].ciso
 		 *
 		 * FST format:
@@ -325,22 +329,17 @@ static DevState LoadGameList(gameinfo *gi, u32 sz, u32 *pGameCount)
 					     GetRootDevice(), filename_utf8);
 
 			//Test if game.iso exists and add to list
-			bool found[2] = {false, false};
+			bool found = false;
 
-			const char disc_filenames[5][16] = {
+			static const char disc_filenames[8][16] = {
 				"game.ciso", "game.cso", "game.gcm", "game.iso",
-				// FIXME: Disc 2 filenames other than "disc2.iso"
-				// aren't supported yet.
-				// Fix kernel/DI.c to handle it.
-				/*"disc2.ciso", "disc2.cso", "disc2.gcm",*/ "disc2.iso"
+				"disc2.ciso", "disc2.cso", "disc2.gcm", "disc2.iso"
 			};
 
 			u32 i;
-			for (i = 0; i < 5; i++)
+			for (i = 0; i < 8; i++)
 			{
 				const u32 discNumber = i / 4;
-				if (found[discNumber])
-					continue;
 
 				// Append the disc filename.
 				strcpy(&filename[fnlen], disc_filenames[i]);
@@ -350,13 +349,15 @@ static DevState LoadGameList(gameinfo *gi, u32 sz, u32 *pGameCount)
 				{
 					// Disc image exists and is a GameCube disc.
 					gamecount++;
-					found[discNumber] = true;
+					found = true;
+					// Next disc number.
+					i = (discNumber * 4) + 3;
 				}
 			}
 
 			// If none of the expected files were found,
 			// check for FST format.
-			if (!found[0] && !found[1])
+			if (!found)
 			{
 				memcpy(&filename[fnlen], "sys/boot.bin", 13);
 				if (f_open_char(&in, filename, FA_READ|FA_OPEN_EXISTING) == FR_OK)

commit 7671f9833349fb3c648bb3f67b7c3b3d363cf83e

[kernel] DI.c: Fix disc swapping if Disc 2 is started initially.
We were only checking if Disc 1 (game.iso) was loaded, so the multi-disc
code was effectively disabled if the user started Disc 2.

This bug was reported by Zonta85 on GBAtemp:
- https://gbatemp.net/threads/usb-loader-gx.149922/page-1163#post-6884844
- https://gbatemp.net/threads/nintendont.349258/page-1758#post-6891955

This fixes a regression from commit 6f67b91031a27ab08b91f3c45a6a335849037d2e.
(Initial attempt at supporting multiple extensions for 2-disc games.)

kernel/DI.c
@@ -89,7 +89,9 @@ static u8 *const DIMMMemory = (u8*)0x12B80000;
 
 // Multi-disc filenames.
 static const char disc_filenames[8][16] = {
+	// Disc 1
 	"game.ciso", "game.cso", "game.gcm", "game.iso",
+	// Disc 2
 	"disc2.ciso", "disc2.cso", "disc2.gcm", "disc2.iso"
 };
 
@@ -164,7 +166,7 @@ void DIinit( bool FirstTime )
 			// Check if this is a 2-disc game.
 			u32 i, slash_pos;
 			char TempDiscName[256];
-			_sprintf(TempDiscName, "%s", ConfigGetGamePath());
+			strcpy(TempDiscName, ConfigGetGamePath());
 
 			//search the string backwards for '/'
 			for (slash_pos = strlen(TempDiscName); slash_pos > 0; --slash_pos)
@@ -174,32 +176,49 @@ void DIinit( bool FirstTime )
 			}
 			slash_pos++;
 
-			// First, make sure the disc's filename is game.(ciso|cso|gcm|iso).
+			// First, check if the disc filename matches
+			// the expected filenames for multi-disc games.
+			int checkIdxMin = -1, checkIdxMax = -1;
+			const char **DI_2disc_otherdisc = NULL;
 			DI_2disc_filenames[0] = NULL;
 			DI_2disc_filenames[1] = NULL;
-			for (i = 0; i < 4; i++)
+			for (i = 0; i < 8; i++)
 			{
 				if (!strcasecmp(TempDiscName+slash_pos, disc_filenames[i]))
 				{
-					// This is game.(ciso|cso|gcm|iso).
-					DI_2disc_filenames[0] = disc_filenames[i];
+					// Filename is either:
+					// -  game.(ciso|cso|gcm|iso) (Disc 1)
+					// - disc2.(ciso|cso|gcm|iso) (Disc 2)
+					const int discIdx = i / 4;	// either 0 or 1
+					DI_2disc_filenames[discIdx] = disc_filenames[i];
+
+					// Set variables to check for the other disc.
+					if (discIdx == 0) {
+						checkIdxMin = 4;
+						checkIdxMax = 7;
+						DI_2disc_otherdisc = &DI_2disc_filenames[1];
+					} else {
+						checkIdxMin = 0;
+						checkIdxMax = 3;
+						DI_2disc_otherdisc = &DI_2disc_filenames[0];
+					}
 					break;
 				}
 			}
 
-			if (DI_2disc_filenames[0] != NULL)
+			if (DI_2disc_otherdisc != NULL)
 			{
-				// Check for the disc2 file.
-				for (i = 4; i < 8; i++)
+				// Check for the other disc.
+				for (i = checkIdxMin; i < checkIdxMax; i++)
 				{
-					_sprintf(TempDiscName+slash_pos, disc_filenames[i]);
+					strcpy(TempDiscName+slash_pos, disc_filenames[i]);
 					FIL ExistsFile;
 					s32 ret = f_open_char(&ExistsFile, TempDiscName, FA_READ);
 					if (ret == FR_OK)
 					{
-						// Found the disc image.
+						// Found the other disc image.
 						f_close(&ExistsFile);
-						DI_2disc_filenames[1] = disc_filenames[i];
+						*DI_2disc_otherdisc = disc_filenames[i];
 						break;
 					}
 				}