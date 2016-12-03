FIX94/Nintendont

commit 7671f9833349fb3c648bb3f67b7c3b3d363cf83e

[kernel] DI.c: Fix disc swapping if Disc 2 is started initially.
We were only checking if Disc 1 (game.iso) was loaded, so the multi-disc
code was effectively disabled if the user started Disc 2.

This bug was reported by Zonta85 on GBAtemp:
- https://gbatemp.net/threads/usb-loader-gx.149922/page-1163#post-6884844
- https://gbatemp.net/threads/nintendont.349258/page-1758#post-6891955

This fixes a regression from commit 6f67b91031a27ab08b91f3c45a6a335849037d2e.
(Initial attempt at supporting multiple extensions for 2-disc games.)

kernel/DI.c
@@ -89,7 +89,9 @@ static u8 *const DIMMMemory = (u8*)0x12B80000;
 
 // Multi-disc filenames.
 static const char disc_filenames[8][16] = {
+	// Disc 1
 	"game.ciso", "game.cso", "game.gcm", "game.iso",
+	// Disc 2
 	"disc2.ciso", "disc2.cso", "disc2.gcm", "disc2.iso"
 };
 
@@ -164,7 +166,7 @@ void DIinit( bool FirstTime )
 			// Check if this is a 2-disc game.
 			u32 i, slash_pos;
 			char TempDiscName[256];
-			_sprintf(TempDiscName, "%s", ConfigGetGamePath());
+			strcpy(TempDiscName, ConfigGetGamePath());
 
 			//search the string backwards for '/'
 			for (slash_pos = strlen(TempDiscName); slash_pos > 0; --slash_pos)
@@ -174,32 +176,49 @@ void DIinit( bool FirstTime )
 			}
 			slash_pos++;
 
-			// First, make sure the disc's filename is game.(ciso|cso|gcm|iso).
+			// First, check if the disc filename matches
+			// the expected filenames for multi-disc games.
+			int checkIdxMin = -1, checkIdxMax = -1;
+			const char **DI_2disc_otherdisc = NULL;
 			DI_2disc_filenames[0] = NULL;
 			DI_2disc_filenames[1] = NULL;
-			for (i = 0; i < 4; i++)
+			for (i = 0; i < 8; i++)
 			{
 				if (!strcasecmp(TempDiscName+slash_pos, disc_filenames[i]))
 				{
-					// This is game.(ciso|cso|gcm|iso).
-					DI_2disc_filenames[0] = disc_filenames[i];
+					// Filename is either:
+					// -  game.(ciso|cso|gcm|iso) (Disc 1)
+					// - disc2.(ciso|cso|gcm|iso) (Disc 2)
+					const int discIdx = i / 4;	// either 0 or 1
+					DI_2disc_filenames[discIdx] = disc_filenames[i];
+
+					// Set variables to check for the other disc.
+					if (discIdx == 0) {
+						checkIdxMin = 4;
+						checkIdxMax = 7;
+						DI_2disc_otherdisc = &DI_2disc_filenames[1];
+					} else {
+						checkIdxMin = 0;
+						checkIdxMax = 3;
+						DI_2disc_otherdisc = &DI_2disc_filenames[0];
+					}
 					break;
 				}
 			}
 
-			if (DI_2disc_filenames[0] != NULL)
+			if (DI_2disc_otherdisc != NULL)
 			{
-				// Check for the disc2 file.
-				for (i = 4; i < 8; i++)
+				// Check for the other disc.
+				for (i = checkIdxMin; i < checkIdxMax; i++)
 				{
-					_sprintf(TempDiscName+slash_pos, disc_filenames[i]);
+					strcpy(TempDiscName+slash_pos, disc_filenames[i]);
 					FIL ExistsFile;
 					s32 ret = f_open_char(&ExistsFile, TempDiscName, FA_READ);
 					if (ret == FR_OK)
 					{
-						// Found the disc image.
+						// Found the other disc image.
 						f_close(&ExistsFile);
-						DI_2disc_filenames[1] = disc_filenames[i];
+						*DI_2disc_otherdisc = disc_filenames[i];
 						break;
 					}
 				}

commit 1af5486feefd4aa3123523d1dbb55b9cebab8f75

[kernel] DI.c: Fix an off-by-one in the 2disc code.
This prevented *.iso from being used with 2disc mode. (*.gcm worked fine.)

This bug was reported by Zonta85 on GBAtemp.

This fixes a regression from commit 7671f9833349fb3c648bb3f67b7c3b3d363cf83e.
([kernel] DI.c: Fix disc swapping if Disc 2 is started initially.)

common/include/NintendontVersion.h
@@ -2,7 +2,7 @@
 #define __NINTENDONT_VERSION_H__
 
 #define NIN_MAJOR_VERSION			4
-#define NIN_MINOR_VERSION			429
+#define NIN_MINOR_VERSION			430
 
 #define NIN_VERSION		((NIN_MAJOR_VERSION << 16) | NIN_MINOR_VERSION)
 

kernel/DI.c
@@ -209,7 +209,7 @@ void DIinit( bool FirstTime )
 			if (DI_2disc_otherdisc != NULL)
 			{
 				// Check for the other disc.
-				for (i = checkIdxMin; i < checkIdxMax; i++)
+				for (i = checkIdxMin; i <= checkIdxMax; i++)
 				{
 					strcpy(TempDiscName+slash_pos, disc_filenames[i]);
 					FIL ExistsFile;