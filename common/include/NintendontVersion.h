FIX94/Nintendont

commit e6e1c6a75683f46f23f13d36cbb11da1635ac2ac

[kernel] Use the BI2 region code instead of the game ID.
Kernel-side portion of the BI2 change.

The SADX Preview now works when the IPL is enabled. Before the loader
change, the 'S' in the game ID caused the loader to not load the IPL
at all; after the loader change, the loader used the JPN IPL, but the
kernel was still using 'S' as the region code, which resulted in the
JPN IPL rejecting the game disc.

kernel/SRAM.c
@@ -2,8 +2,6 @@
 #include "SRAM.h"
 #include "Config.h"
 
-extern u32 Region;
-
 // Initial SRAM values.
 GC_SRAM sram ALIGNED(32) =
 {
@@ -73,58 +71,72 @@ void SRAM_Init(void)
 	sram.Flags    &= ~0x80;	// Clear Progmode
 	sram.Flags    &= ~3;	// Clear Videomode
 	sram.DisplayOffsetH = 0;
-
-	const u32 GameID = ConfigGetGameID();
-	switch (GameID & 0xFF)
-	{
-		case 'E':
-		case 'J':
-			//BMX XXX doesnt even boot on a real gc with component cables
-			if ((ConfigGetGameID() >> 8) != 0x474233 &&
-			    (ConfigGetVideoMode() & NIN_VID_PROG))
-			{
-				sram.Flags |= 0x80;	// Set Progmode
-			}
-			break;
-		default:
-			sram.BootMode	|= 0x40;	// Set PAL60
-			break;
-	}
-
 	sram.Language = ConfigGetLanguage();
 
+	// Apply settings based on the actual game region.
+	u32 tmp_BI2region = BI2region;
+	if (tmp_BI2region == BI2_REGION_PAL)
+	{
+		// Enable PAL60.
+		sram.BootMode |= 0x40;
+
+		// TODO: Set the progressive scan flag on PAL?
+	}
+	else
+	{
+		// Disable PAL60.
+		sram.BootMode &= 0x40;
+
+		// Set the progressive scan flag if a component cable
+		// is connected (or HDMI on Wii U), unless we're loading
+		// BMX XXX, since that game won't even boot on a real
+		// GameCube if a component cable is connected.
+		if ((ncfg->GameID >> 24) != 0x474233 &&
+		    (ncfg->VideoMode & NIN_VID_PROG))
+		{
+			sram.Flags |= 0x80;
+		}
+	}
+
+	// Are we forcing a video mode?
 	if( ConfigGetVideoMode() & NIN_VID_FORCE )
 	{
 		switch( ConfigGetVideoMode() & NIN_VID_FORCE_MASK )
 		{
 			case NIN_VID_FORCE_NTSC:
-			{
-				Region = 0;
-			} break;
+				// Force NTSC.
+				tmp_BI2region = BI2_REGION_JAPAN;
+				break;
+
 			case NIN_VID_FORCE_MPAL:
 			case NIN_VID_FORCE_PAL50:
 			case NIN_VID_FORCE_PAL60:
-			{
-				Region = 2;
-			} break;
+				// Force PAL.
+				tmp_BI2region = BI2_REGION_PAL;
+				break;
+
+			default:
+				break;
 		}
 	}
 
-	switch(Region)
+	// Check for PAL60 with the forced video mode.
+	switch (tmp_BI2region)
 	{
+		case BI2_REGION_JAPAN:
+		case BI2_REGION_USA:
+		case BI2_REGION_SOUTH_KOREA:
 		default:
-		case 0:
-		case 1:
-		{
+			// NTSC
 #ifdef DEBUG_EXI
 			dbgprintf("SRAM:NTSC\r\n");
 #endif
 			*(vu32*)0xCC = 0;
-			
-		} break;
-		case 2:
-		{
-			if( *(vu32*)0xCC == 5 )
+			break;
+
+		case BI2_REGION_PAL:
+			// PAL
+			if (*(vu32*)0xCC == 5)
 			{
 #ifdef DEBUG_EXI
 				dbgprintf("SRAM:PAL60\r\n");
@@ -135,10 +147,11 @@ void SRAM_Init(void)
 #endif
 				*(vu32*)0xCC = 1;
 			}
-			sram.Flags		|= 1;
-
-		} break;
+			sram.Flags |= 1;
+			break;
 	}
+
+	sync_after_write((void*)0xCC, 4);
 
 	// Update the SRAM checksum.
 	SRAM_UpdateChecksum();

kernel/main.c
@@ -261,12 +261,23 @@ int _main( int argc, char *argv[] )
 	write32( HW_PPCIRQMASK, (1<<30) );
 	write32( HW_PPCIRQFLAG, read32(HW_PPCIRQFLAG) );
 
-//This bit seems to be different on japanese consoles
+	//This bit seems to be different on japanese consoles
 	u32 ori_ppcspeed = read32(HW_PPCSPEED);
-	if((ConfigGetGameID() & 0xFF) == 'J')
-		set32(HW_PPCSPEED, (1<<17));
-	else
-		clear32(HW_PPCSPEED, (1<<17));
+	switch (BI2region)
+	{
+		case BI2_REGION_JAPAN:
+		case BI2_REGION_SOUTH_KOREA:
+		default:
+			// JPN games.
+			set32(HW_PPCSPEED, (1<<17));
+			break;
+
+		case BI2_REGION_USA:
+		case BI2_REGION_PAL:
+			// USA/PAL games.
+			clear32(HW_PPCSPEED, (1<<17));
+			break;
+	}
 
 	// Set the Wii U widescreen setting.
 	u32 ori_widesetting = 0;

kernel/Config.c
@@ -2,6 +2,9 @@
 #include "debug.h"
 #include "ff_utf8.h"
 
+// BI2.bin region code.
+u32 BI2region = 0;
+
 void ConfigSyncBeforeRead( void )
 {
 	sync_before_read(ncfg, sizeof(NIN_CFG));

kernel/Config.h
@@ -43,6 +43,9 @@ enum SystemLanguage {	Japanese,
 void ConfigSyncBeforeRead( void );
 void ConfigInit( void );
 
+// BI2.bin region code.
+extern u32 BI2region;
+
 // Nintendont configuration.
 static NIN_CFG *const ncfg = (NIN_CFG*)0x13002900;
 

kernel/EXI.c
@@ -37,7 +37,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 static u32 CurrentTiming = EXI_IRQ_DEFAULT;
 
-extern u32 Region;
 extern vu32 useipl;
 
 static u32 Device = 0;
@@ -83,10 +82,24 @@ void EXIInit( void )
 		memcpy(MemCardName, "/saves/", 7);
 		if ( ConfigGetConfig(NIN_CFG_MC_MULTI) )
 		{
-			if ((GameID & 0xFF) == 'J')  // JPN games
-				memcpy(MemCardName+7, "ninmemj.raw", 11);
-			else
-				memcpy(MemCardName+7, "ninmem.raw", 10);
+			// "Multi" mode is enabled.
+			// Use one memory card for USA/PAL games,
+			// and another memory card for JPN games.
+			switch (BI2region)
+			{
+				case BI2_REGION_JAPAN:
+				case BI2_REGION_SOUTH_KOREA:
+				default:
+					// JPN games.
+					memcpy(MemCardName+7, "ninmemj", 7);
+					break;
+
+				case BI2_REGION_USA:
+				case BI2_REGION_PAL:
+					// USA/PAL games.
+					memcpy(MemCardName+7, "ninmem", 6);
+					break;
+			}
 		}
 		else
 		{
@@ -140,6 +153,12 @@ void EXIInit( void )
 }
 
 extern vu32 TRIGame;
+
+/**
+ * Set EXI timings based on the game ID.
+ * @param TitleID Game ID, rshifted by 8.
+ * @param Region Region byte from Game ID.
+ */
 void EXISetTimings(u32 TitleID, u32 Region)
 {
 	//GC BIOS, Trifoce Game, X-Men Legends 2, Rainbow Six 3 or Starfox Assault (NTSC-U)

kernel/EXI.h
@@ -83,7 +83,14 @@ enum EXICommands {
 #define EXI_READWRITE	2 
 
 void EXIInit();
+
+/**
+ * Set EXI timings based on the game ID.
+ * @param TitleID Game ID, rshifted by 8.
+ * @param Region Region byte from Game ID.
+ */
 void EXISetTimings(u32 TitleID, u32 Region);
+
 bool EXICheckTimer();
 void EXIInterrupt();
 void EXIUpdateRegistersNEW( void );

kernel/FST.c
@@ -79,8 +79,6 @@ static u32 FCEntry = 0;
 static FileCache *FC;
 static u32 FCState[FILECACHE_MAX];
 
-extern u32 Region;
-
 u32 FSTInit( const char *GamePath )
 {
 	char Path[256];
@@ -336,7 +334,8 @@ const u8* FSTRead(const char *GamePath, u32* Length, u32 Offset)
 
 			f_close( &fd );
 
-			Region = *(vu32*)(DI_READ_BUFFER+0x18);
+			// BI2.bin region code.
+			BI2region = *(vu32*)(DI_READ_BUFFER+0x18);
 
 			return DI_READ_BUFFER;
 		}

commit fe0b79c9df487e29b38ff43b5a0929d4f88cbb89

-fixed bmx xxx with 480p yet again (broken since commit e6e1c6a)

common/include/NintendontVersion.h
@@ -2,7 +2,7 @@
 #define __NINTENDONT_VERSION_H__
 
 #define NIN_MAJOR_VERSION			4
-#define NIN_MINOR_VERSION			437
+#define NIN_MINOR_VERSION			438
 
 #define NIN_VERSION		((NIN_MAJOR_VERSION << 16) | NIN_MINOR_VERSION)
 

kernel/SRAM.c
@@ -91,7 +91,7 @@ void SRAM_Init(void)
 		// is connected (or HDMI on Wii U), unless we're loading
 		// BMX XXX, since that game won't even boot on a real
 		// GameCube if a component cable is connected.
-		if ((ncfg->GameID >> 24) != 0x474233 &&
+		if ((ncfg->GameID >> 8) != 0x474233 &&
 		    (ncfg->VideoMode & NIN_VID_PROG))
 		{
 			sram.Flags |= 0x80;

loader/source/main.c
@@ -1270,7 +1270,7 @@ int main(int argc, char **argv)
 			// is connected (or HDMI on Wii U), unless we're loading
 			// BMX XXX, since that game won't even boot on a real
 			// GameCube if a component cable is connected.
-			if ((ncfg->GameID >> 24) != 0x474233 &&
+			if ((ncfg->GameID >> 8) != 0x474233 &&
 			    (ncfg->VideoMode & NIN_VID_PROG))
 			{
 				sram->flags |= 0x80;