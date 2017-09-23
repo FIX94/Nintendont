ea94056a6cc482beecdde88e256cffcf79e10a0c
FIX94 <fix94.1@gmail.com>
2017-09-23 23:18:31 +0200

    recompile after merge

--- common/include/NintendontVersion.h
+++ common/include/NintendontVersion.h
@@ -2,7 +2,7 @@
 #define __NINTENDONT_VERSION_H__
 
 #define NIN_MAJOR_VERSION			5
-#define NIN_MINOR_VERSION			462
+#define NIN_MINOR_VERSION			463
 
 #define NIN_VERSION		((NIN_MAJOR_VERSION << 16) | NIN_MINOR_VERSION)
 
@@ -12,6 +12,6 @@
 
 // "Special" version.
 // This should only be set in custom builds, i.e. not mainline.
-#define NIN_SPECIAL_VERSION			"-MChax3"
+//#define NIN_SPECIAL_VERSION			"-MChax3"
 
 #endif
