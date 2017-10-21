8a7d3f266087b61cbfc58260a1c418176ea8cc4d
FIX94 <fix94.1@gmail.com>
2017-10-21 20:07:41 +0200

    properly handle the vwii case of having no bootable game on selected device

--- loader/source/menu.c
+++ loader/source/menu.c
@@ -47,12 +47,14 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 // Device state.
 typedef enum {
-	// Device is open and has a "games" directory.
+	// Device is open and has GC titles in "games" directory.
 	DEV_OK = 0,
 	// Device could not be opened.
 	DEV_NO_OPEN = 1,
 	// Device was opened but has no "games" directory.
 	DEV_NO_GAMES = 2,
+	// Device was opened but "games" directory is empty.
+	DEV_NO_TITLES = 3,
 } DevState;
 static u8 devState = DEV_OK;
 
@@ -292,9 +294,10 @@ static bool IsDiscImageValid(const char *filename, int discNumber, gameinfo *gi)
  * @param pGameCount   [out] Number of games loaded. (Includes GCN pseudo-game for Wii.)
  *
  * @return DevState value:
- * - DEV_OK: Device opened and has a "games/" directory.
+ * - DEV_OK: Device opened and has GC titles in "games/" directory.
  * - DEV_NO_OPEN: Could not open the storage device.
  * - DEV_NO_GAMES: No "games/" directory was found.
+ * - DEV_NO_TITLES: "games/" directory contains no GC titles.
  */
 static DevState LoadGameList(gameinfo *gi, u32 sz, u32 *pGameCount)
 {
@@ -500,7 +503,7 @@ static DevState LoadGameList(gameinfo *gi, u32 sz, u32 *pGameCount)
 	// Sort the list alphabetically.
 	// On Wii, the pseudo-entry for GameCube discs is always
 	// kept at the top.
-	if( IsWiiU() && !isWiiVC )
+	if( gamecount && IsWiiU() && !isWiiVC )
 		qsort(gi, gamecount, sizeof(gameinfo), compare_names);
 	else if( gamecount > 1 )
 		qsort(&gi[1], gamecount-1, sizeof(gameinfo), compare_names);
@@ -508,6 +511,9 @@ static DevState LoadGameList(gameinfo *gi, u32 sz, u32 *pGameCount)
 	// Save the game count.
 	if (pGameCount)
 		*pGameCount = gamecount;
+
+	if(gamecount == 0)
+		return DEV_NO_TITLES;
 
 	return DEV_OK;
 }
@@ -537,6 +543,7 @@ typedef struct _MenuCtx
 		const gameinfo *gi;	// Game information.
 		int gamecount;		// Game count.
 
+		bool canBeBooted;	// Selected game is bootable.
 		bool canShowInfo;	// Can show information for the selected game.
 	} games;
 
@@ -631,8 +638,11 @@ static bool UpdateGameSelectMenu(MenuCtx *ctx)
 				ctx->games.posX	= 0;
 				ctx->games.scrollX = 0;
 			}
-		} else {
+		} else if(ctx->games.listMax) {
 			ctx->games.posX = ctx->games.listMax - 1;
+		}
+		else {
+			ctx->games.posX	= 0;
 		}
 
 		clearCheats = true;
@@ -654,7 +664,12 @@ static bool UpdateGameSelectMenu(MenuCtx *ctx)
 				ctx->games.scrollX--;
 			} else {
 				// Wraparound.
-				ctx->games.posX	= ctx->games.listMax - 1;
+				if(ctx->games.listMax) {
+					ctx->games.posX = ctx->games.listMax - 1;
+				}
+				else {
+					ctx->games.posX	= 0;
+				}
 				ctx->games.scrollX = ctx->games.gamecount - ctx->games.listMax;
 			}
 		} else {
@@ -692,7 +707,7 @@ static bool UpdateGameSelectMenu(MenuCtx *ctx)
 		ctx->saveSettings = true;
 	}
 
-	if (FPAD_OK(0))
+	if (FPAD_OK(0) && ctx->games.canBeBooted)
 	{
 		// User selected a game.
 		ctx->selected = true;
@@ -758,30 +773,40 @@ static bool UpdateGameSelectMenu(MenuCtx *ctx)
 			}
 		}
 
-		// Can we show information for the selected title?
-		if (IsWiiU() && !isWiiVC) {
-			// Can show information for all games on WiiU
-			ctx->games.canShowInfo = true;
-		} else {
-			if ((ctx->games.scrollX + ctx->games.posX) == 0) {
-				// Cannot show information for DISC01 on Wii and Wii VC.
-				ctx->games.canShowInfo = false;
-			} else {
-				// Can show information for all other games.
+		if(ctx->games.gamecount && (ctx->games.scrollX + ctx->games.posX) >= 0 
+			&& (ctx->games.scrollX + ctx->games.posX) < ctx->games.gamecount)
+		{
+			ctx->games.canBeBooted = true;
+			// Can we show information for the selected title?
+			if (IsWiiU() && !isWiiVC) {
+				// Can show information for all games on WiiU
 				ctx->games.canShowInfo = true;
+			} else {
+				if ((ctx->games.scrollX + ctx->games.posX) == 0) {
+					// Cannot show information for DISC01 on Wii and Wii VC.
+					ctx->games.canShowInfo = false;
+				} else {
+					// Can show information for all other games.
+					ctx->games.canShowInfo = true;
+				}
+			}
+
+			if (ctx->games.canShowInfo) {
+				// Print the selected game's filename.
+				const gameinfo *const gi = &ctx->games.gi[ctx->games.scrollX + ctx->games.posX];
+				const int len = strlen(gi->Path);
+				const int x = (640 - (len*10)) / 2;
+
+				const u32 color = DiscFormatColors[gi->Flags & GIFLAG_FORMAT_MASK];
+				PrintFormat(DEFAULT_SIZE, color, x, MENU_POS_Y + 20*4+5, "%s", gi->Path);
 			}
 		}
-
-		if (ctx->games.canShowInfo) {
-			// Print the selected game's filename.
-			const gameinfo *const gi = &ctx->games.gi[ctx->games.scrollX + ctx->games.posX];
-			const int len = strlen(gi->Path);
-			const int x = (640 - (len*10)) / 2;
-
-			const u32 color = DiscFormatColors[gi->Flags & GIFLAG_FORMAT_MASK];
-			PrintFormat(DEFAULT_SIZE, color, x, MENU_POS_Y + 20*4+5, "%s", gi->Path);
+		else
+		{
+			//invalid title selected
+			ctx->games.canBeBooted = false;
+			ctx->games.canShowInfo = false;
 		}
-
 		// GRRLIB rendering is done by SelectGame().
 	}
 
@@ -1581,6 +1606,13 @@ static int SelectGame(void)
 			gprintf("WARNING: %s:/games/ was not found.\n", GetRootDevice());
 			break;
 
+		case DEV_NO_TITLES:
+			// "games" directory appears to be empty.
+			// The list will still be shown, since there's a
+			// "Boot GC Disc in Drive" option on Wii.
+			gprintf("WARNING: %s:/games/ contains no GC titles.\n", GetRootDevice());
+			break;
+
 		case DEV_NO_OPEN:
 		default:
 		{
@@ -1682,9 +1714,12 @@ static int SelectGame(void)
 			if (ctx.menuMode == 0)
 			{
 				// Game List menu.
-				PrintButtonActions("Go Back", "Select", "Settings", NULL);
+				PrintButtonActions("Go Back", NULL, "Settings", NULL);
+				// If the selected game bootable, enable "Select".
+				u32 color = ((ctx.games.canBeBooted) ? BLACK : DARK_GRAY);
+				PrintFormat(DEFAULT_SIZE, color, MENU_POS_X + 430, MENU_POS_Y + 20*1, "A   : Select");
 				// If the selected game is not DISC01, enable "Game Info".
-				const u32 color = ((ctx.games.canShowInfo) ? BLACK : DARK_GRAY);
+				color = ((ctx.games.canShowInfo) ? BLACK : DARK_GRAY);
 				PrintFormat(DEFAULT_SIZE, color, MENU_POS_X + 430, MENU_POS_Y + 20*3, "X/1 : Game Info");
 			}
 			else
@@ -1709,17 +1744,19 @@ static int SelectGame(void)
 		}
 	}
 
-	// Save the selected game to the configuration.
-	u32 SelectedGame = ctx.games.posX + ctx.games.scrollX;
-	const char* StartChar = gi[SelectedGame].Path + 3;
-	if (StartChar[0] == ':') {
-		StartChar++;
+	if(ctx.games.canBeBooted)
+	{
+		// Save the selected game to the configuration.
+		u32 SelectedGame = ctx.games.posX + ctx.games.scrollX;
+		const char* StartChar = gi[SelectedGame].Path + 3;
+		if (StartChar[0] == ':') {
+			StartChar++;
+		}
+		strncpy(ncfg->GamePath, StartChar, sizeof(ncfg->GamePath));
+		ncfg->GamePath[sizeof(ncfg->GamePath)-1] = 0;
+		memcpy(&(ncfg->GameID), gi[SelectedGame].ID, 4);
+		DCFlushRange((void*)ncfg, sizeof(NIN_CFG));
 	}
-	strncpy(ncfg->GamePath, StartChar, sizeof(ncfg->GamePath));
-	ncfg->GamePath[sizeof(ncfg->GamePath)-1] = 0;
-	memcpy(&(ncfg->GameID), gi[SelectedGame].ID, 4);
-	DCFlushRange((void*)ncfg, sizeof(NIN_CFG));
-
 	// Free allocated memory in the game list.
 	for (i = 0; i < gamecount; ++i)
 	{
@@ -1899,6 +1936,10 @@ static void PrintDevInfo(void)
 		case DEV_NO_GAMES:
 			PrintFormat(DEFAULT_SIZE, MAROON, MENU_POS_X, MENU_POS_Y + 20*4,
 				"WARNING: %s:/games/ was not found.", GetRootDevice());
+			break;
+		case DEV_NO_TITLES:
+			PrintFormat(DEFAULT_SIZE, MAROON, MENU_POS_X, MENU_POS_Y + 20*4,
+				"WARNING: %s:/games/ contains no GC titles.", GetRootDevice());
 			break;
 		default:
 			break;
