6811017ab19537dfd98e40260c0f64fa755c7ae1
meta <hosaka-corp@users.noreply.github.com>
2018-08-12 13:46:17 -0500

    accept() and sendto() working
    * Turns out we weren't using the right IOCTL number on listen() - ha.
    * Reserved a little bit more stack space for net thread
    * accept() and sendto() seem to work.

--- kernel/net.c
+++ kernel/net.c
@@ -108,7 +108,7 @@ s32 listen(s32 fd, s32 socket, u32 backlog)
 	params[0] = socket;
 	params[1] = backlog;
 
-	res = IOS_Ioctl(fd, IOCTL_SO_BIND, params, 8, 0, 0);
+	res = IOS_Ioctl(fd, IOCTL_SO_LISTEN, params, 8, 0, 0);
 
 	dbgprintf("listen(%d, %d, %d) = %d\r\n", fd, socket, backlog, res);
 	return res;
@@ -136,7 +136,7 @@ s32 accept(s32 fd, s32 socket)
 	return res;
 }
 
-s32 sendto(s32 fd, s32 socket, void *data, s32 len, u32 flags, struct sockaddr *to)
+s32 sendto(s32 fd, s32 socket, void *data, s32 len, u32 flags)
 {
 	s32 res;
 	STACK_ALIGN(struct sendto_params, params, 1, 32);
@@ -161,8 +161,8 @@ s32 sendto(s32 fd, s32 socket, void *data, s32 len, u32 flags, struct sockaddr *
 
 	params->socket = socket;
 	params->flags = flags;
-	params->has_destaddr = 1; // ?
-	memcpy(params->destaddr, to, to->sa_len);
+	params->has_destaddr = 0; // ?
+	//memcpy(params->destaddr, to, to->sa_len);
 
 	vec[0].data = message_buf;
 	vec[0].len = len;
@@ -174,8 +174,8 @@ s32 sendto(s32 fd, s32 socket, void *data, s32 len, u32 flags, struct sockaddr *
 	if (message_buf != NULL)
 		heap_free(0, message_buf);
 
-	dbgprintf("sendto(%d, %d, %p, %d, %d, %p) = %d\r\n",
-			fd, socket, data, len, flags, to);
+	dbgprintf("sendto(%d, %d, %p, %d, %d) = %d\r\n",
+			fd, socket, data, len, flags);
 
 	return res;
 }
@@ -204,29 +204,30 @@ s32 connect(s32 fd, s32 socket, struct address *addr)
 }
 
 
-static struct sockaddr_in server;
-static int sock;
-static s32 top_fd;
+static struct sockaddr_in server __attribute__((aligned(32)));
+static int sock __attribute__((aligned(32)));
+static int client_sock __attribute__((aligned(32)));
+static s32 top_fd __attribute__((aligned(32)));
 
 u32 net_handler(void *arg)
 {
 	s32 res;
 	dbgprintf("net_handler TID: %d\r\n", thread_get_id());
-
-	
-	int client_sock;
 	u32 my_message = 0xdeadc0de;
 
 	while (1)
 	{
 		mdelay(10);
 		client_sock = accept(top_fd, sock);
-		// dbgprintf("client:sock: %d\r\n", client_sock);
 		if (client_sock >= 0) {
-			dbgprintf("accept returned %d\r\n", client_sock);
-			close(top_fd, client_sock);
-			break;
+			while (1) 
+			{
+				sendto(top_fd, client_sock, &my_message, 4, 0);
+				close(top_fd, client_sock);
+				break;
+			}
 		}
+
 	}
 	return 0;
 }
@@ -246,15 +247,6 @@ int NetInit(void)
 	IOS_Ioctl(top_fd, IOCTL_SO_STARTUP, 0, 0, 0, 0);
 	dbgprintf("top_fd: %d\r\n", top_fd);
 	if (top_fd < 0) return -1;
-
-	// int ip = 0;
-	// do
-	// {
-	// 	mdelay(1000);
-	// 	ip = IOS_Ioctl(top_fd, 16, 0, 0, 0, 0);
-	// } while(ip <= 0);
-	// dbgprintf("ip: 0x%x\n", ip);
-
 
 	sock = socket(top_fd, AF_INET, SOCK_STREAM, IPPROTO_IP);
 	dbgprintf("sock: %d\r\n", sock);
