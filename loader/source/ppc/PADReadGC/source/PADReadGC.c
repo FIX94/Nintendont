8da722064e63f5610972d98dd883639d0a0a3bb6
James Crowley <James.Crowley1@marist.edu>
2021-02-28 12:48:45 -0500

    Super Smash Bros Ultimate GC Controllers Drift Patch (#868)
    * Adding in patches based on akimasa patch
    
    * Increased version and uploaded new loader.dol with patch

--- loader/source/ppc/PADReadGC/source/PADReadGC.c
+++ loader/source/ppc/PADReadGC/source/PADReadGC.c
@@ -42,6 +42,11 @@ static u32 PrevAdapterChannel3 = 0;
 static u32 PrevAdapterChannel4 = 0;
 static u32 PrevDRCButton = 0;
 
+static u8 OffsetX[NIN_CFG_MAXPAD] = {0};
+static u8 OffsetY[NIN_CFG_MAXPAD] = {0};
+static u8 OffsetCX[NIN_CFG_MAXPAD] = {0};
+static u8 OffsetCY[NIN_CFG_MAXPAD] = {0};
+
 #define DRC_SWAP (1<<16)
 
 const s8 DEADZONE = 0x1A;
@@ -292,6 +297,17 @@ u32 PADRead(u32 calledByGame)
 			{
 				goto DoExit;
 			}
+			if((Pad[chan].button&0x1c00) == 0x1c00 || ((*PadUsed & (1 << chan)) == 0))
+			{
+				OffsetX[chan] = Pad[chan].stickX;
+				OffsetY[chan] = Pad[chan].stickY;
+				OffsetCX[chan] = Pad[chan].substickX;
+				OffsetCY[chan] = Pad[chan].substickY;
+			}
+			Pad[chan].stickX -= OffsetX[chan];
+			Pad[chan].stickY -= OffsetY[chan];
+			Pad[chan].substickX -= OffsetCX[chan];
+			Pad[chan].substickY -= OffsetCY[chan];
 			if((Pad[chan].button&0x1030) == 0x1030)	//reset by pressing start, Z, R
 			{
 				/* reset status 3 */
@@ -546,6 +562,17 @@ u32 PADRead(u32 calledByGame)
 			stickY		= HID_Packet[HID_CTRL->StickY.Offset] - 128;	//raw EE ED EC ... 82 81 80 7F 7E ... 1A 19 18 (up, center, down)
 			substickX	= HID_Packet[HID_CTRL->CStickX.Offset] - 128;	//raw 22 23 24 ... 7F 80 81 ... D2 D3 D4 (left ... center ... right)
 			substickY	= HID_Packet[HID_CTRL->CStickY.Offset] - 128;	//raw DB DA D9 ... 81 80 7F ... 2B 2A 29 (up, center, down)
+			if((Pad[chan].button&0x1c00) == 0x1c00 || ((*PadUsed & (1 << chan)) == 0))
+			{
+				OffsetX[chan] = stickX;
+				OffsetY[chan] = stickY;
+				OffsetCX[chan] = substickX;
+				OffsetCY[chan] = substickY;
+			}
+			stickX -= OffsetX[chan];
+			stickY -= OffsetY[chan];
+			substickX -= OffsetCX[chan];
+			substickY -= OffsetCY[chan];
 		}
 		else	//standard sticks
 		{
