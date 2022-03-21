403c4103416d94f8c54b3d802a764f1f345f5672
cyberstudio10 <13622461+cyberstudio10@users.noreply.github.com>
2022-03-21 09:58:33 -0700

    Second place with a list of contributors, used by main.c to write a new meta.xml. Have to maintain 2 copies for now, merging them is pretty difficult. Also, there has never been any mechanism for main.c to write the date to meta.xml, and this is what we will need to look into.

--- common/include/Metadata.h
+++ common/include/Metadata.h
@@ -5,8 +5,8 @@
 #define META_NAME			"Nintendont"
 #define META_AUTHOR			"FIX94, crediar"
 
-#define META_LONG1			"Commiters: GerbilSoft, JoostinOnline, GreyRogue, Howard, Cyan \r\n\r\n Project website: https://github.com/FIX94/Nintendont "
-#define META_LONG2			"Nintendont allows you to run GameCube games on a Wii or Wii U from an SD or HDD device."
+#define META_LONG1  		"Nintendont allows you to run GameCube games on a Wii or Wii U from an SD or HDD device."
+#define META_LONG2		    " Project website: https://github.com/FIX94/Nintendont\r\n\r\nCommitters: GerbilSoft, JoostinOnline, GreyRogue, Howard, Cyan, nastys, carnage702, AnthonyRyuki, DankRank, Chickoodel, akemin-dayo, SuperrSonic, KoldMonster12, cheatfreak47, capnfunky77, cesarmades, crazycaveman, Wolfy76700, AndyCasaceli, ShadowOne333, Aurelio92, darkalemanbr, jmapjp, sailormoon, Dr-Crow, cyberstudio10, corey-underdown"
 #define META_SHORT			"Gamecube Loader"
 
 #define META_XML			"<?xml version=\"1.0\" encoding=\"ISO-8859-1\" standalone=\"yes\"?>"
