2ab8549134ef0498f25559dc2addd0d90a65516c
cyberstudio10 <13622461+cyberstudio10@users.noreply.github.com>
2022-03-21 17:47:19 -0700

    When loader writes its own meta.xml it is now able to write the current date/time, which is passed by make as META_DATETIME. If that variable is missing the existing 2016 timestamp is used.

--- common/include/Metadata.h
+++ common/include/Metadata.h
@@ -4,7 +4,9 @@
 
 #define META_NAME			"Nintendont"
 #define META_AUTHOR			"FIX94, crediar"
-
+#if !defined(META_DATETIME)
+#   define META_DATETIME    "20160710000000"
+#endif
 #define META_LONG1  		"Nintendont allows you to run GameCube games on a Wii or Wii U from an SD or HDD device."
 #define META_LONG2		    " Project website: https://github.com/FIX94/Nintendont\r\n\r\nCommitters: GerbilSoft, JoostinOnline, GreyRogue, Howard, Cyan, nastys, carnage702, AnthonyRyuki, DankRank, Chickoodel, akemin-dayo, SuperrSonic, KoldMonster12, cheatfreak47, capnfunky77, cesarmades, crazycaveman, Wolfy76700, AndyCasaceli, ShadowOne333, Aurelio92, darkalemanbr, jmapjp, sailormoon, Dr-Crow, cyberstudio10, corey-underdown"
 #define META_SHORT			"Gamecube Loader"
