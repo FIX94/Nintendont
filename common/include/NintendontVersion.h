fe1d5848df969326fdda7814f337687695dd3332
libertyernie <maybeway36@gmail.com>
2023-12-09 16:18:36 -0600

    Update NIN_SPECIAL_VERSION

--- common/include/NintendontVersion.h
+++ common/include/NintendontVersion.h
@@ -10,19 +10,21 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 #define NIN_VERSION_STRING "$$Version:" STRINGIZE(NIN_MAJOR_VERSION) "." STRINGIZE(NIN_MINOR_VERSION)
 
-// "Special" version.
-// This should only be set in custom builds, i.e. not mainline.
-#define NIN_SPECIAL_VERSION			"-libertyernie"
-
 #define LI_XBOX360
 #define LI_NONUNCHUK
+#define LI_SHOULDER
+#define LI_ANALOG_SHOULDER_FULL
 #define LI_GAMEPADASCCPRO
 #define LI_NOSWAP
 #define LI_NOEXIT
 #define LI_NORESET
 #define LI_CUSTOM_CONTROLS
 #define LI_BASE64
-#define LI_SHOULDER
-#define LI_ANALOG_SHOULDER_FULL
+
+#if defined(LI_XBOX360)
+#define NIN_SPECIAL_VERSION			"-li-green"
+#else if (!defined(LI_XBOX360) && !defined(LI_NONUNCHUK))
+#define NIN_SPECIAL_VERSION			"-li-blue"
+#endif
 
 #endif
